05b3f44df6b6f5f48194bfd607edbfa9dd237e6f
srli <sophia.li@students.olin.edu>
2015-12-15 15:22:59 -0500

    added smile detection

--- scripts/sight/face_detect_pub.py
+++ scripts/sight/face_detect_pub.py
@@ -1,66 +1,50 @@
 #!/usr/bin/python
-
 import cv2
 import time
 import Image
-import roslib; roslib.load_manifest('jimmy')
+import roslib; roslib.load_manifest('edwin')
 import rospy
 from std_msgs.msg import String
 import numpy as np
 
-class Face:
-    def __init__(self,x,y,w,h):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.midx = x + w/2
-        self.midy = y + h/2
+class FaceFinder:
+    def __init__(self):
+        rospy.init_node("face_finder", anonymous=True)
+        self.pub = rospy.Publisher('behavior_cmd', String, queue_size=10)
 
-def largestFaceIndex(faces):
-    largestIndex = 0;
-    largest = 0
-    for index, face in enumerate(faces):
-        if face.w > largest:
-            largest = face.w
-            largestIndex = index
-    return largestIndex
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml') #face xml
+        self.mouth_cascade = cv2.CascadeClassifier('haarcascade_smile.xml') #smile xml
 
-def publisher():
-    rospy.init_node("face_location",anonymous = True)
-    
-    cap = cv2.VideoCapture(1)
-    cap.set(3, 480)
-    cap.set(4, 360)
+    def find_smile(self):
+        ret, self.frame = self.cap.read()
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        real_faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minSize=(20,20))
 
-    face_cascade = cv2.CascadeClassifier('/home/skelly1/opencv-2.4.10/data/haarcascades/haarcascade_frontalface_alt.xml')
+        if len(real_faces) != 0:
+            for (x,y,w,h) in real_faces:
+                print "FACE"
+                self.pub.publish("greet")
+                cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = self.frame[y:y+h, x:x+w]
+                mouth = self.mouth_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=20, minSize=(10,10), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+                for (mp,mq,mr,ms) in mouth:
+                    cv2.rectangle(roi_color,(mp,mq),(mp+mr,mq+ms), (255,0,0),1)
+                    print "SMILE"
 
-    pub = rospy.Publisher('face_location', String, queue_size = 10)
-    while not rospy.is_shutdown():
-        ret, frame = cap.read()
+        cv2.imshow("output", self.frame)
+        c = cv2.waitKey(1)
 
-        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
-        if len(faces) > 0:
-            faceObjects = []
-            for (x, y, w, h) in faces:
-                faceObjects.append(Face(x,y,w,h))
 
-            for (x,y,w,h) in faces:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
 
-            face = faceObjects[largestFaceIndex(faceObjects)]
-            print "largest face at:"+str(face.midx)+", "+str(face.midy)
-            pub.publish(str(face.midx)+","+str(face.midy))
-        else:
-            print "no face detected: returning (x,y) = (300,250)"
-            pub.publish(str(300)+","+str(250))
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    def run(self):
+        self.cap = cv2.VideoCapture(0)
+        r = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.find_smile()
+            r.sleep()
 
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    publisher()
+    face_finder = FaceFinder()
+    face_finder.run()
