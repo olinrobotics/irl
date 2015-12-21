f3fcc7e48a46b5469366faa7fdc80173ee584d6e
Maxmilian626 <maxmilian.wei@students.olin.edu>
2015-12-20 22:09:39 -0500

    AddedHoughCircles, rectangle finder can now be used to find central rectangle

--- scripts/sight/FieldScanTest.py
+++ scripts/sight/FieldScanTest.py
@@ -9,7 +9,7 @@ def FieldScan():
 	#I suppose FieldScan works on the assumption that the human doesn't cheat.
 	#Technically, Edwin will look for the grid, and assign each box a number in the
 	#array.
-	cap = cv2.VideoCapture(0)
+	cap = cv2.VideoCapture(1)
 	while True:
 		#This program should find the large tic tac toe box,break it down into
 		#9 different cells, and then analyze the contents of each cell.  
@@ -21,21 +21,28 @@ def FieldScan():
 
 		contours,h = cv2.findContours(blur,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
 
-		for cont in contours:
-			approx = cv2.approxPolyDP(cont, 0.01*cv2.arcLength(cont, True), True)
-			sides = len(approx)
-			if (sides > 15):
-				c, r = cv2.minEnclosingCircle(cont)
-				c2 = np.round(c).astype("int")
-				radius = np.round(r).astype("int")
-				area = cv2.contourArea(cont)
-				filterLevel = int(area - 3.14*radius**2)
-				if filterLevel < 2:
-					cv2.circle(frame, (c2[0], c2[1]), radius, (0, 255, 0), 4)
-			elif sides == 4:
-				cv2.drawContours(frame, [cont], 0, (0, 255, 255), -1)
+		circles = cv2.HoughCircles(imgray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
+		if circles is not None:
+		# convert the (x, y) coordinates and radius of the circles to integers
+			circles = np.round(circles[0, :]).astype("int")
+ 
+			# loop over the (x, y) coordinates and radius of the circles
+			for (x, y, r) in circles:
+			# draw the circle in the output image, then draw a rectangle
+			# corresponding to the center of the circle
+				cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+		else:
 
-		#Still needs a return function.  
+			for cont in contours:
+				approx = cv2.approxPolyDP(cont, 0.01*cv2.arcLength(cont, True), True)
+				sides = len(approx)
+				if sides == 4:
+					x,y,w,h = cv2.boundingRect(cont)
+					cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+					#cv2.drawContours(frame, [cont], 0, (0, 255, 255), -1)
+
+
+		 
 
 
 		cv2.imshow("camera", frame)
@@ -45,3 +52,4 @@ def FieldScan():
 
 if __name__ == '__main__':
 	FieldScan()
+
