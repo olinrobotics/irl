f9c9381041e3da3b0855bcfec98191e1abf5b11f
srli <sophia.li@students.olin.edu>
2016-02-03 17:30:06 -0500

    watershed

--- scripts/sight/watershed.py
+++ scripts/sight/watershed.py
@@ -2,18 +2,11 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('test_imgs/6.jpg')
+img = cv2.imread('test_imgs/2a.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-
-# sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
-
-cv2.imshow('img', sure_bg)
+cv2.imshow('img', thresh)
 c = cv2.waitKey(0)
 cv2.destroyAllWindows()
 
