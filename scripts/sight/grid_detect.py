7f929277def17526641bc5541759cc71facd86ab
srli <sophia.li@students.olin.edu>
2016-02-05 16:49:22 -0500

    board detection

--- scripts/sight/grid_detect.py
+++ scripts/sight/grid_detect.py
@@ -1,98 +1,139 @@
+#!/usr/bin/env python
+import rospy
 import cv2
+import cv2.cv as cv
 import numpy as np
-from matplotlib import pyplot as plt
+import random
+import time
 import operator
 import math
 import itertools
 
-def collinear(p0, p1, p2):
-    x1, y1 = p1[0] - p0[0], p1[1] - p0[1]
-    x2, y2 = p2[0] - p0[0], p2[1] - p0[1]
-    return x1 * y2 - x2 * y1 < 1e-12
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from edwin.msg import Edwin_Shape
 
-def get_distance(pt1, pt2):
-	return math.sqrt((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)
+class GridFinder:
+	def __init__(self):
+		rospy.init_node('grid_locator', anonymous = True)
+		self.draw_pub = rospy.Publisher('draw_cmd', Edwin_Shape, queue_size=10)
+		self.arm_pub = rospy.Publisher('arm_cmd', String, queue_size=10)
 
-def get_pt_x(pt1, pt2, d):
-	v = (pt1[0]-pt2[0], pt1[1]-pt2[1])
-	v_mag = get_distance(pt1, pt2)
+		#Board X and Y positions
+		self.b_x = 0
+		self.b_y = 4000
+		self.b_w = 250
 
-	u = (v[0]/v_mag, v[1]/v_mag)
+		self.board_msg = Edwin_Shape()
+		self.draw_msg = Edwin_Shape()
 
-	return (int(pt1[0]+d*u[0]), int(pt1[1]+d*u[1]))
+	def get_distance(self, pt1, pt2):
+		return math.sqrt((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)
 
-def get_box(ref_box, h, w):
-	pt0 = ref_box[0]
-	pt1 = ref_box[1]
-	pt2 = get_pt_x(ref_box[1], ref_box[2], h)
-	pt3 = get_pt_x(ref_box[0], ref_box[3], h)
+	def get_pt_x(self, pt1, pt2, d):
+		v = (pt1[0]-pt2[0], pt1[1]-pt2[1])
+		v_mag = self.get_distance(pt1, pt2)
 
-	return [pt0, pt1, pt2, pt3]
+		u = (v[0]/v_mag, v[1]/v_mag)
 
-def get_grid(box):
-	grid_height = int(get_distance(box[0], box[1]))
-	grid_width = int(get_distance(box[1], box[3]))
+		return (int(pt1[0]+d*u[0]), int(pt1[1]+d*u[1]))
 
-	box2 = get_box([box[2], box[1], box[0], box[3]], grid_height, grid_width)
-	box4 = get_box(box, grid_height, grid_width)
-	box5 = get_box([box[3], box[2], box[1], box[0]], grid_height, grid_width)
-	box7 = get_box([box[3], box[0], box[1], box[2]], grid_height, grid_width)
+	def get_box(self, ref_box, h, w):
+		pt0 = ref_box[0]
+		pt1 = ref_box[1]
+		pt2 = self.get_pt_x(ref_box[1], ref_box[2], h)
+		pt3 = self.get_pt_x(ref_box[0], ref_box[3], h)
 
-	box1 = get_box([box4[2], box4[1], box4[0], box4[3]], grid_height, grid_width)
-	box6 = get_box([box4[3], box4[0], box4[1], box4[2]], grid_height, grid_width)
+		return [pt0, pt1, pt2, pt3]
 
-	box3 = get_box([box5[2], box5[1], box5[0], box5[3]], grid_height, grid_width)
-	box8 = get_box([box5[3], box5[0], box5[1], box5[2]], grid_height, grid_width)
+	def get_grid(self, box):
+		grid_height = int(self.get_distance(box[0], box[1]))
+		grid_width = int(self.get_distance(box[1], box[3]))
 
-	return [box1, box2, box3, box4, box5, box6, box7, box8]
+		box2 = self.get_box([box[2], box[1], box[0], box[3]], grid_height, grid_width)
+		box4 = self.get_box(box, grid_height, grid_width)
+		box5 = self.get_box([box[3], box[2], box[1], box[0]], grid_height, grid_width)
+		box7 = self.get_box([box[3], box[0], box[1], box[2]], grid_height, grid_width)
 
-def get_center_box(im_in):
-	# img = cv2.imread(im_in)
-	img = im_in
-	h, w, ch = img.shape
+		box1 = self.get_box([box4[2], box4[1], box4[0], box4[3]], grid_height, grid_width)
+		box6 = self.get_box([box4[3], box4[0], box4[1], box4[2]], grid_height, grid_width)
 
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		box3 = self.get_box([box5[2], box5[1], box5[0], box5[3]], grid_height, grid_width)
+		box8 = self.get_box([box5[3], box5[0], box5[1], box5[2]], grid_height, grid_width)
 
-	corners = cv2.goodFeaturesToTrack(gray,25,0.01,120)
-	corners = np.int0(corners)
+		return [box1, box2, box3, box4, box5, box6, box7, box8]
 
-	dists = {}
-	for num, i in enumerate(corners):
-		x,y = i.ravel()
-		cv2.circle(img,(x,y),3,255,-1)
-		dists[num] = math.sqrt((x-(w/2))**2 + (y-(h/2))**2)
+	def get_center_box(self, im_in):
+		# img = cv2.imread(im_in)
+		img = im_in
+		h, w, ch = img.shape
 
-	sorted_dists = sorted(dists.items(), key=operator.itemgetter(1))
-	center_rect = []
+		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-	pts_distances = []
+		corners = cv2.goodFeaturesToTrack(gray,25,0.01,120)
+		corners = np.int0(corners)
 
-	for i in range(4):
-		pts_distances.append(sorted_dists[i])
-		center_rect.append(list(corners[pts_distances[i][0]].ravel()))
+		dists = {}
+		for num, i in enumerate(corners):
+			x,y = i.ravel()
+			cv2.circle(img,(x,y),3,255,-1)
+			dists[num] = math.sqrt((x-(w/2))**2 + (y-(h/2))**2)
 
-	print center_rect
+		sorted_dists = sorted(dists.items(), key=operator.itemgetter(1))
+		center_rect = []
 
-	rect = cv2.minAreaRect(np.int0(center_rect))
+		pts_distances = []
 
-	box = cv2.cv.BoxPoints(rect)
-	box = np.int0(box)
+		for i in range(4):
+			pts_distances.append(sorted_dists[i])
+			center_rect.append(list(corners[pts_distances[i][0]].ravel()))
 
-	boxes = get_grid(box)
+		# print center_rect
 
-	for box in boxes:
+		rect = cv2.minAreaRect(np.int0(center_rect))
+
+		box = cv2.cv.BoxPoints(rect)
 		box = np.int0(box)
-		cv2.drawContours(img,[box],0,(0,0,255),2)
 
-	return img
+		boxes = self.get_grid(box)
 
-def main():
-	cap = cv2.VideoCapture(1)
-	while True:
-		ret, frame = cap.read()
-		img = get_center_box(frame)
-		cv2.imshow("img", frame)
-		c = cv2.waitKey(1)
+		for box in boxes:
+			box = np.int0(box)
+			cv2.drawContours(img,[box],0,(0,0,255),2)
+
+		return img
+
+	def draw_the_board(self):
+		print "drawing the board"
+		self.board_msg.shape = "board"
+		self.board_msg.x = self.b_x
+		self.board_msg.y = self.b_y
+		#note that Z should be a function of y.
+		self.board_msg.z = -805 - ((self.board_msg.y - 2500)/10)
+		self.draw_pub.publish(self.board_msg)
+
+		time.sleep(25)
+
+		#pick marker off paper
+		msg = "data: move_to:: 200, 2700, 1000, 0"
+		# msg = "data: move_to:: " + str(self.b_x) + ", " + str(self.b_y) + ", " + str(self.board_msg.z+250) + ", " + str(0)
+		print "sending: ", msg
+		self.arm_pub.publish(msg)
+		time.sleep(1)
+
+	def run(self):
+		cap = cv2.VideoCapture(1)
+		time.sleep(5)
+		self.draw_the_board()
+
+		# r = rospy.Rate(10)
+		while not rospy.is_shutdown():
+			ret, frame = cap.read()
+			img = self.get_center_box(frame)
+			cv2.imshow("img", frame)
+			c = cv2.waitKey(1)
+			# r.sleep()
 
 if __name__ == "__main__":
-	main()
+	gf = GridFinder()
+	gf.run()
