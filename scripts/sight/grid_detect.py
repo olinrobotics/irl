fe0d8505a2c2392f3bb565f569ed08ddc0a5a170
srli <sophia.li@students.olin.edu>
2016-02-06 17:50:34 -0500

    todo: fix in-box check

--- scripts/sight/grid_detect.py
+++ scripts/sight/grid_detect.py
@@ -16,6 +16,12 @@ from cv_bridge import CvBridge, CvBridgeError
 
 
 def inside_rect(pt1, pt2, pt3, x, y):
+	ax = pt1[0]
+	ay = pt1[1]
+	bx = pt2[0]
+	by = pt2[1]
+	dx = pt1[0]
+	dy = pt2[0]
 	bax = pt2[0] - pt1[0]
 	bay = pt2[1] - pt1[1]
 	dax = pt3[0] - pt1[0]
@@ -95,24 +101,25 @@ class GridDetector:
 
 		pts_distances = []
 
-		for i in range(4):
-			pts_distances.append(sorted_dists[i])
-			center_rect.append(list(corners[pts_distances[i][0]].ravel()))
+		if len(sorted_dists) > 4:
+			for i in range(4):
+				pts_distances.append(sorted_dists[i])
+				center_rect.append(list(corners[pts_distances[i][0]].ravel()))
 
-		# print center_rect
+			# print center_rect
 
-		rect = cv2.minAreaRect(np.int0(center_rect))
+			rect = cv2.minAreaRect(np.int0(center_rect))
 
-		box = cv2.cv.BoxPoints(rect)
-		box = np.int0(box)
-
-		boxes = self.get_grid(box)
-
-		for box in boxes:
+			box = cv2.cv.BoxPoints(rect)
 			box = np.int0(box)
-			# cv2.drawContours(img,[box],0,(0,0,255),2)
 
-		self.boxes = np.int0(boxes)
+			boxes = self.get_grid(box)
+
+			for box in boxes:
+				box = np.int0(box)
+				# cv2.drawContours(img,[box],0,(0,0,255),2)
+
+			self.boxes = np.int0(boxes)
 		return corners
 
 	def run(self, img):
@@ -137,6 +144,15 @@ class GridTester:
 		self.draw_msg = Edwin_Shape()
 
 
+		#A blank space is represented by 0, an "O" is 1, and "X" is 10, we start with blank board
+		self.board =   [0, 0, 0,
+						0, 0, 0,
+						0, 0, 0]
+
+		self.corners = [0,2,6,8] #indices of the corner locations
+		self.sides = [1,3,5,7]
+		self.middle = 4
+
 	def img_callback(self, data):
 		try:
 			self.frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -146,9 +162,8 @@ class GridTester:
 
 	def field_scan(self):
 		time.sleep(5)
-
 		#pick marker off paper
-		msg = "data: move_to:: 200, 2700, 1000, 0"
+		msg = "data: move_to:: 150, 2400, 1500, 0"
 		# msg = "data: move_to:: " + str(self.b_x) + ", " + str(self.b_y) + ", " + str(self.board_msg.z+250) + ", " + str(0)
 		print "sending: ", msg
 		self.arm_pub.publish(msg)
@@ -170,97 +185,95 @@ class GridTester:
 			# ret, self.frame = self.cap.read()
 
 			gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-			blur = cv2.GaussianBlur(gray, (5, 5), 0)
+			# blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
 			height, width, channels = self.frame.shape
 			self.image_w = width/3
 			self.image_h = height/3
 
-			contours,h = cv2.findContours(blur,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
-			circles = cv2.HoughCircles(blur, cv.CV_HOUGH_GRADIENT,1, 100, param1=50,param2=30,minRadius=20,maxRadius=50)
-			print len(circles)
+			# contours,h = cv2.findContours(blur,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
+			circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT,1, 100, param1=50,param2=30,minRadius=20,maxRadius=50)
 			if circles is not None:
 				circles = np.uint16(np.around(circles))
-
 				circles = np.round(circles[0, :]).astype("int")
 				for (x, y, r) in circles:
 					cv2.circle(self.frame, (x, y), r, (0, 255, 0), 4)
-					for i in range(9):
+					for i in range(8):
 						if self.board[i] == 0:
 							#checks each section of box if it contains a circle
-							if inside_rect(self.image_rectangles[i][0], self.image_rectangles[i][1], self.image_rectangles[i][2], self.image_rectangles[i][3], int(x), int(y)):
+							if inside_rect(self.image_rectangles[i][0], self.image_rectangles[i][1], self.image_rectangles[i][3], int(x), int(y)):
 								try:
 									new_index[i] += 1
 								except KeyError:
 									new_index[i] = 1
 								continue
+				print self.board
+
+				for key in new_index.keys():
+					if new_index[key] > 5:
+						self.board[key] = 1
+						running = False
+
+			else:
+				running = False
 
 
-			# for rect in self.image_rectangles:
-				# cv2.rectangle(self.frame, rect, (rect[0]+self.image_w, rect[1]+self.image_h), (0,0,255), 2)
+			for box in self.image_rectangles:
+				box = np.int0(box)
+				cv2.drawContours(self.frame,[box],0,(0,0,255),2)
 
-			# cv2.imshow("camera", self.frame)
-			# c = cv2.waitKey(1)
-
-			for key in new_index.keys():
-				if new_index[key] == 5:
-					self.board[key] = 1
-					running = False
+			cv2.imshow("img", self.frame)
+			c = cv2.waitKey(1)
 
 	def draw_the_board(self):
-		# print "drawing the board"
-		# self.board_msg.shape = "board"
-		# self.board_msg.x = self.b_x
-		# self.board_msg.y = self.b_y
-		# #note that Z should be a function of y.
-		# self.board_msg.z = -805 - ((self.board_msg.y - 2500)/10)
-		# self.draw_pub.publish(self.board_msg)
+		print "drawing the board"
+		self.board_msg.shape = "board"
+		self.board_msg.x = self.b_x
+		self.board_msg.y = self.b_y
+		#note that Z should be a function of y.
+		self.board_msg.z = -670 - int((self.board_msg.y - 2500)/9)
+		# self.board_msg.z = -300
+		self.draw_pub.publish(self.board_msg)
+		print self.board_msg
+		time.sleep(25)
 
-		# time.sleep(25)
-
-		# #look at grid
-		## msg = "data: move_to:: 200, 2700, 1000, 0"
-		# msg = "data: move_to:: 150, 2400, 1500, 0"
-		# print "sending: ", msg
-		# self.arm_pub.publish(msg)
-		# time.sleep(1)
+		#look at grid
+		# msg = "data: move_to:: 200, 2700, 1000, 0"
+		msg = "data: move_to:: 150, 2400, 1500, 0"
+		print "sending: ", msg
+		self.arm_pub.publish(msg)
+		time.sleep(1)
 
 		gd = GridDetector(self.frame)
 		corners = gd.get_center_box()
 		self.image_rectangles = gd.boxes
 
-		# print "CORNERS: ", corners
-		# print "RECTANGLES: ", self.image_rectangles
-
-		for i in range(20):
+		for i in range(30):
 			gd.run(self.frame)
 			corners = gd.get_center_box()
 			for j in range(8):
 				elem = gd.boxes[j]
 				self.image_rectangles[j] = (self.image_rectangles[j] + elem)/2
 
-			# self.image_rectangles = (np.array(self.image_rectangles) + np.array(gd.boxes))/2
+		# for box in self.image_rectangles:
+		# 	box = np.int0(box)
+		# 	cv2.drawContours(self.frame,[box],0,(0,0,255),2)
 
+		# for num, i in enumerate(corners):
+		# 	x,y = i.ravel()
+		# 	cv2.circle(self.frame,(x,y),3,255,-1)
 
-		for box in self.image_rectangles:
-			box = np.int0(box)
-			cv2.drawContours(self.frame,[box],0,(0,0,255),2)
-
-		for num, i in enumerate(corners):
-			x,y = i.ravel()
-			cv2.circle(self.frame,(x,y),3,255,-1)
-
-		cv2.imshow("img", self.frame)
-		c = cv2.waitKey(1)
+		# cv2.imshow("img", self.frame)
+		# c = cv2.waitKey(1)
 
 		# print self.image_rectangles
 
 	def run(self):
 		time.sleep(2)
 		# # r = rospy.Rate(10)
+		self.draw_the_board()
 		while not rospy.is_shutdown():
-			self.draw_the_board()
-		# 	self.field_scan()
+			self.field_scan()
 		# 	# ret, frame = cap.read()
 		# 	# img = self.get_center_box(frame)
 			# cv2.imshow("img", self.frame)
