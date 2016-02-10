e57a92968c9fa0abdd7d94ba1b5e7ac5a03ae35b
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-02-10 16:40:23 -0500

    Got the file opening and playback working

--- scripts/sound/soundboard.py
+++ scripts/sound/soundboard.py
@@ -1,54 +1,46 @@
 import rospy
-import math
-import st
-import numpy as np
 from std_msgs.msg import String
 from edwin.msg import *
 import time
+import subprocess
 
 class AudioObject:
-    def __init__(self, name):
-        self.name = name
-        self.path = "../media" # TODO: fix this so it doesn't use '..'
-        self.filename = "{}/{}.mp3".format(self.path, self.name)
-        self.player = 'mplayer'
-        self.playme = False
+	def __init__(self, name):
+		self.name = name
+		self.path = "media" # TODO: fix this so it doesn't use '..'
+		self.filename = "{}/{}.wav".format(self.path, self.name)
+		self.player = 'mplayer'
 
-    def play_wave(self):
-        """
-        plays an inputted wav file
-        """
-        print self.filename
-        cmd = '{} {}'.format(self.player, self.filename)
-        #popen = subprocess.Popen([self.player, self.filename, "-ss", "30"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	def play_wave(self):
+		"""
+		plays an inputted wav file
+		"""
+		print self.filename
+		cmd = '{} {}'.format(self.player, self.filename)
+		#popen = subprocess.Popen([self.player, self.filename, "-ss", "30"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        popen = subprocess.Popen(cmd, shell=True)
-        popen.communicate()
-        return 1
-        #popen.stdin.write("q")
-
-    def update(self):
-        if self.playme:
-            time.sleep(1)
-            print "playing sound file ", self.filename
-            self.play_wave()
-            self.playme = False
-        return 1
+		popen = subprocess.Popen(cmd, shell=True)
+		popen.communicate()
+		return 1
+		#popen.stdin.write("q")
 
 class SoundBoard:
 	def __init__(self):
-        rospy.init_node('edwin_sounds', anonymous = True)
-        rospy.Subscriber('/sound_cmd', Edwin_Sound, self.sound_callback, queue_size=10)
-        self.behavior_pub = rospy.Publisher('behaviors_cmd', String, queue_size=10)
-        self.sound_library = [] #Array of audio objects, to be added as needed.
-        self.create_objects()
+		rospy.init_node('edwin_sounds', anonymous = True)
+		self.sound_library =  self.create_objects()
 
-    def create_objects(self):
-        pass
+	def create_objects(self): #Reads all the files in media, instantiates them as audio_objects
+		AudioList = []
+		AudioList.append(AudioObject("Battlecry")) #Example/test file
+		
+		return AudioList
 
-    def sound_callback(self, data):
-    	call = data.msg #String indicating desired sound
-		command = (x for x in self.sound_library if x.name == call) #Find in sound library
+	def sound_callback(self, data):
+		call =  data.msg #String indicating desired sound
+		command = next((x for x in self.sound_library if x.name == call)) #Find in sound library
 		command.play_wave()
 		return 1
 
+if __name__ == '__main__':
+	sound = SoundBoard()
+	sound.sound_callback()
