3d3c89348f68e19041ce1caa1ce68e4f3739d561
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-03-25 17:44:07 -0400

    changed shape detect into class structure

--- scripts/sight/shape_detection.py
+++ scripts/sight/shape_detection.py
@@ -4,16 +4,14 @@ import cv2
 import math
 
 
+def detect_shape(picture):
 
-
-while(True):
-
-    img = cv2.imread('square.jpg')  
+    img = cv2.imread(picture)  
 
     imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(imgray,127,255,1)
     contours, h = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-     
+
     for cnt in contours:
 
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
@@ -23,6 +21,7 @@ while(True):
 
             print "square"   
             cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+            return approx
 
         """elif len(approx)==5:
             print "pentagon"
@@ -43,7 +42,19 @@ while(True):
             cv2.drawContours(img,[cnt],0,(0,255,255),-1)"""
 
     cv2.imshow('frame',img)
+
+#def has_moved():
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    print detect_shape('square.jpg')
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.destroyAllWindows()
 
 cv2.destroyAllWindows()
