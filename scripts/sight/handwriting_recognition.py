07254d3a44da0af285ba357ee3c1cb6e8e3d5d48
Matt Brucker <mpbrucker@gmail.com>
2016-10-22 16:33:13 -0400

    Created Character class, unified handling of stream frame

--- scripts/sight/handwriting_recognition.py
+++ scripts/sight/handwriting_recognition.py
@@ -8,12 +8,11 @@ from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 
 import numpy as np
+from Character import Character
 
 import cv2
 
 class HandwritingRecognition:
-    def nothing(x):
-        pass
 
     def __init__(self):
         self.img = cv2.imread('test_imgs/digits.png')
@@ -24,8 +23,6 @@ class HandwritingRecognition:
         PACKAGE_PATH = rospack.get_path("edwin")
         self.detect = True
         cv2.namedWindow('image')
-        cv2.createTrackbar('K','image',1,255,self.nothing)
-        cv2.setTrackbarPos('K','image',5)
         cv2.setMouseCallback('image',self.fill_test_data)
         self.test_data = np.zeros((200,200),np.uint8)
         self.test_filled = 0
@@ -45,19 +42,21 @@ class HandwritingRecognition:
         data_img = cv2.imread('test_data.png')
         gray = cv2.cvtColor(data_img,cv2.COLOR_BGR2GRAY)
         cells = [np.hsplit(row,10) for row in np.vsplit(gray,10)]
-        x = np.array(cells)
-        test = x[:,:].reshape(-1,400).astype(np.float32)
-        labels = np.array([[8],[9],[5],[3],[4],[8],[7],[3],[1],[2],
-                           [8],[7],[9],[2],[3],[4],[8],[9],[0],[3],
-                           [4],[5],[5],[4],[1],[2],[7],[9],[7],[6],
-                           [5],[2],[3],[1],[5],[2],[7],[8],[5],[7],
-                           [8],[4],[5],[7],[8],[4],[5],[1],[2],[6],
-                           [5],[4],[2],[2],[1],[9],[8],[8],[5],[4],
-                           [8],[7],[7],[4],[4],[3],[2],[3],[4],[1],
-                           [5],[2],[9],[7],[8],[8],[7],[5],[4],[1],
-                           [2],[8],[7],[5],[4],[1],[7],[7],[7],[9],
-                           [8],[8],[7],[8],[7],[1],[6],[4],[8],[6]])
-        ret,result,neighbors,dist = self.knn.find_nearest(test,k=2)
+        deskewed = [map(self.deskew,row) for row in cells]
+        # print deskewed[0][0]
+        hogdata = [map(self.hog,row) for row in deskewed]
+        test_data = np.float32(hogdata).reshape(-1,64)
+        labels = np.array([[9],[8],[5],[6],[2],[3],[7],[8],[4],[5],
+                           [1],[9],[7],[8],[4],[5],[1],[3],[2],[9],
+                           [7],[8],[6],[5],[2],[3],[9],[8],[5],[6],
+                           [7],[8],[5],[7],[8],[5],[7],[8],[5],[9],
+                           [7],[8],[6],[5],[2],[3],[9],[7],[8],[5],
+                           [2],[3],[7],[8],[5],[5],[1],[2],[7],[8],
+                           [4],[5],[7],[8],[5],[9],[8],[7],[6],[5],
+                           [2],[3],[9],[8],[7],[6],[5],[2],[3],[7],
+                           [8],[4],[5],[7],[8],[4],[5],[8],[7],[8],
+                           [4],[5],[1],[2],[7],[8],[4],[5],[1],[2]])
+        result = self.SVM.predict_all(test_data)
         matches = result==labels
         correct = np.count_nonzero(matches)
         accuracy = correct*100.0/result.size
@@ -65,79 +64,58 @@ class HandwritingRecognition:
 
     def img_callback(self, data):
         try:
-            self.frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.curr_frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-    def process_data(self):
+    def process_data_svm(self):
+        cells_data = []
         gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
         cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
-        x = np.array(cells)
+        # print cells[0][0]
+        deskewed = [map(self.deskew,row) for row in cells]
+        # print deskewed[0][0]
+        hogdata = [map(self.hog,row) for row in deskewed]
+        train_data = np.float32(hogdata).reshape(-1,64)
+        responses = np.float32(np.repeat(np.arange(10),500))[:,np.newaxis]
+        np.savez('svm_data.npz',train=train_data,train_labels=responses)
 
-        train = x[:,:].reshape(-1,400).astype(np.float32)
+    def train_svm(self):
+        svm_params = dict(kernel_type = cv2.SVM_LINEAR, svm_type = \
+                            cv2.SVM_C_SVC, C=2.67, gamma=5.383)
 
-        k = np.arange(10)
-        train_labels = np.repeat(k,500)[:,np.newaxis]
+        with np.load('svm_data.npz') as input_data:
+            train_data = input_data['train']
+            data_labels = input_data['train_labels']
 
-        np.savez('knn_data.npz', train=train, train_labels=train_labels)
-
-    def train_knn(self):
-        with np.load('knn_data.npz') as input_data:
-            self.train_data = input_data['train']
-            self.data_labels = input_data['train_labels']
-
-        self.knn = cv2.KNearest()
-        self.knn.train(self.train_data,self.data_labels)
-
-    def find_text_area(self):
-        img = self.frame
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray,(5,5),0)
-
-        lines = cv2.HoughLines(edges,1,np.pi/180,140)
-        if lines is not None:
-            for rho,theta in lines[0]:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*rho
-                y0 = b*rho
-                x1 = int(x0+1000*(-b))
-                y1 = int(y0+1000*(a))
-                x2 = int(x0-1000*(-b))
-                y2 = int(y0-1000*(a))
-
-                cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-        cv2.imshow('image2',edges)
-        self.frame = img
+        self.SVM = cv2.SVM()
+        self.SVM.train(train_data,data_labels,params=svm_params)
 
         # Returns a list of image ROIs (20x20) corresponding to digits found in the image
     def get_text_roi(self):
-        bound = 15
-        new_frame = self.frame
+        chars = []
+        bound = 5
         kernel = np.ones((2,2),np.uint8)
 
-
-        frame_gray = cv2.cvtColor(new_frame,cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
         frame_gray = cv2.GaussianBlur(frame_gray, (5,5),0) # Gaussian blur to remove noise
 
         # Adaptive threshold to find numbers on paper
         thresh = cv2.adaptiveThreshold(frame_gray,255,
-            cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,31,3)
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,35,7)
         thresh = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=3)
-
+        cv2.imshow('thresh',thresh)
 
         contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,
                                                 cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(self.frame,contours,-1,(255,0,0),2)
         # Build the list of number contours and number locations
 
-
-        self.number_locs = []
-        number_contours = []
-        if len(number_contours) < 100:
+        if len(contours) < 35:
             for ind,contour in enumerate(contours):
                 # print hierarchy[0][ind]
                 [x,y,w,h] = cv2.boundingRect(contour)
-                if  bound < x < (frame_gray.shape[0] - bound) and bound < y < (frame_gray.shape[1] - bound) and (x+w) <= (frame_gray.shape[0] - bound) and (y+h) <= (frame_gray.shape[1] - bound):
+                if  bound < x < (frame_gray.shape[1] - bound) and bound < y < (frame_gray.shape[0] - bound) and (x+w) <= (frame_gray.shape[1] - bound) and (y+h) <= (frame_gray.shape[0] - bound):
                     roi = frame_gray[y-bound:y+h+bound,x-bound:x+w+bound]
 
                     if len(roi) > 0: # Gets rid of weird zero-size contours
@@ -145,73 +123,85 @@ class HandwritingRecognition:
                             cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,31,3)
                         new_roi = cv2.dilate(new_roi,kernel,iterations=2) # Embiggen numbers
                         new_roi = cv2.resize(new_roi, (20,20), interpolation=cv2.INTER_AREA) # standardize contours
-
+                        deskewed = self.deskew(new_roi)
                         # Record contour and contour location, and filter out internal contours
                         if hierarchy[0][ind][3] == -1:
-                            number_contours.append(new_roi)
-                            self.number_locs.append((x+w,y+h))
+                            cont = Character(deskewed,(x,y,w,h), self.hog(deskewed))
+                            chars.append(cont)
 
         # Build an image to show all number contours
-        self.numbers = number_contours
-        if len(number_contours) < 10 and len(number_contours) > 0:
-            new_img = np.ones((20,20*len(number_contours)),np.uint8)
+        num_len = len(chars)
+        # print '# of contours: ', num_len
+        if num_len < 10 and num_len > 0:
+            new_img = np.ones((20,20*num_len),np.uint8)
             y = 0
-            for x in range(0,len(number_contours)):
-                new_img[:,y:y+20] = number_contours[x]
+            for x in chars:
+                new_img[:,y:y+20] = x.img
                 y += 20
             cv2.imshow('image3',new_img)
-        data_array = np.array(number_contours)
-
+        return chars
         # Reshapes the array to be an array of 1x400 floats to fit
         # with the kNN training data
-        try:
-            out_data = data_array[:,:].reshape(-1,400).astype(np.float32)
-            return out_data
-        except IndexError:
-            print 'Something went wrong.'
-        cv2.waitKey(1)
-        return None
+
+    def deskew(self,img):
+        SZ = 20
+        affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
+        m = cv2.moments(img)
+        if abs(m['mu02']) < 1e-2:
+            return img.copy()
+        # print m
+        skew = m['mu11']/m['mu02']
+        M = np.float32([[1,skew,-0.5*SZ*skew], [0,1,0]])
+        img = cv2.warpAffine(img,M,(SZ,SZ),flags=affine_flags)
+        return img
+
+    def hog(self,img):
+        bin_n = 16
+        gx = cv2.Sobel(img, cv2.CV_32F,1,0) # x gradient
+        gy = cv2.Sobel(img, cv2.CV_32F,0,1) # y gradient
+        mag,ang = cv2.cartToPolar(gx,gy) # Polar gradients
+        bins = np.int32(bin_n*ang/(2*np.pi)) # creating binvalues
+        bin_cells = bins[:10,:10],bins[10:,:10],bins[:10,10:],bins[10:,10:]
+        mag_cells = mag[:10,:10],mag[10:,:10],mag[:10,10:],mag[10:,10:]
+        hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+        hist = np.hstack(hists) # hist is a 64-bit vector
+        return hist
+
 
     def process_digits(self,test_data):
-        if test_data is not None:
-            k_val = cv2.getTrackbarPos('K','image')
-            ret,result,neighbors,dist = self.knn.find_nearest(test_data,k=2)
-            new_frame = self.frame
-            self.res = result
-            nums = []
-            if result.shape[0] < 7:
-                for x in self.res:
-                    nums.append(str(int(x.item(0))))
-                for index,y in enumerate(nums):
-                    cv2.putText(new_frame,y,self.number_locs[index],cv2.FONT_HERSHEY_SIMPLEX, 4,(0,255,0))
+        if len(test_data) != 0:
+            # print 'test data length: ', len(test_data)
+            # k_val = cv2.getTrackbarPos('K','image')
+            # ret,result,neighbors,dist = self.knn.find_nearest(test_data,k=2)
+            reshape_data = np.float32([char.HOG for char in test_data]).reshape(-1,64)
+
+            result = self.SVM.predict_all(reshape_data)
+            res_data = []
+            for x in result:
+                res_data.append(int(x.item(0)))
+            for idx,roi in enumerate(self.chars):
+                roi.result = res_data[idx]
+                cv2.putText(self.frame,str(roi.result),(roi.x+roi.w,roi.y+roi.h) \
+                            ,cv2.FONT_HERSHEY_SIMPLEX, 4,(0,255,0))
+
+    def update_frame(self):
+        self.frame = self.curr_frame
 
     def output_image(self):
         new_frame = self.frame
         cv2.imshow('image', new_frame)
-        if self.test_data is not None:
-            cv2.imshow('test', self.test_data)
         cv2.waitKey(1)
-
-    def deskew(self,img):
-        m = cv2.moments(img)
-        if abs(m['mu02']) < 1e-2:
-            return img.copy()
-        skew = m['null']/m['mu02']
-        M = np.float32([[1,skew,-0.5*SZ*skew], [0,1,0]])
-        img = cv2.warpAffine(img,M,(SZ,SZ),flags=affine_flags)
 
     def run(self):
         r = rospy.Rate(10)
         time.sleep(2)
-        self.process_data()
-        self.train_knn()
+        self.process_data_svm()
+        self.train_svm()
         self.test_ocr()
         while not rospy.is_shutdown():
-            #self.find_text_area()
-            ROI = self.get_text_roi()
-            #print ROI.shape
-            self.process_digits(ROI)
-            #cv2.imshow('image2',ROI[0].reshape(20,20).astype(float8))
+            self.update_frame()
+            self.chars = self.get_text_roi()
+            self.process_digits(self.chars)
             self.output_image()
             r.sleep()
 
