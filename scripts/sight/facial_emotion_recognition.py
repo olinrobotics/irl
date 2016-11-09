b471191fa573e90fda95b2c92792b7633f918326
snielsen221b <sophianielsen221b@gmail.com>
2016-11-09 16:00:20 -0500

    CHANGE: add smile haar cascade to facial_emotion_recognition

---
+++ scripts/sight/facial_emotion_recognition.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+import rospkg
+import rospy
+
+import cv2
+import sys
+import logging as log
+import datetime as dt
+
+from time import sleep
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class FaceDetect:
+
+    def __init__(self):
+
+        #detection boolean
+        self.detect = True
+
+        #initializes frame
+        self.frame = None
+
+        #initializes ros node for face detect, pubilishes to face location
+        rospy.init_node('face_detect', anonymous=True)
+        self.pub = rospy.Publisher('/face_location', String, queue_size=10)
+
+        #definines cascade path
+        # cascPath = sys.argv[1]
+        # self.face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        rospack = rospkg.RosPack()
+        PACKAGE_PATH = rospack.get_path("edwin")
+        self.face_cascade = cv2.CascadeClassifier(PACKAGE_PATH + '/params/haarcascade_frontalface_alt.xml')
+        self.smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+        #CvBridge to usb_cam, subscribes to usb cam
+        self.bridge = CvBridge()
+        rospy.Subscriber("usb_cam/image_raw", Image, self.img_callback)
+
+        print "FaceDetect is running"
+
+    #converts ros message to numpy
+    def img_callback(self, data):
+        try:
+            self.frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            h, w = self.frame.shape[:2]
+        except CvBridgeError as e:
+            print(e)
+
+    #face_detect method
+    def face_detect(self):
+        if self.frame == None:
+            return
+
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
+        faces = self.face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(20, 20),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+
+        #draws rectangle around face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # #displays resulting frame
+        cv2.imshow('Video', self.frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.detect = False
+        pass
+
+    def smile_detect(self):
+        if self.frame == None:
+            return
+
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
+        # source for smile code: http://pushbuttons.io/blog/2015/4/27/smile-detection-in-python-opencv
+        smile = self.smile_cascade.detectMultiScale(
+            gray,
+            scaleFactor= 1.7,
+            minNeighbors=22,
+            minSize=(25, 25),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+
+        for (x, y, w, h) in smile:
+            print "Found", len(smile), "smiles!"
+            cv2.rectangle(self.frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
+
+        # #displays resulting frame
+        cv2.imshow('Video', self.frame)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            if self.detect:
+                self.face_detect()
+                self.smile_detect()
+
+if __name__ == '__main__':
+    fd = FaceDetect()
+    #fd.face_detect()
+    fd.run()
