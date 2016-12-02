d1b6c030b3e86081366cc8ae939ac8e45fdd9b67
snielsen221b <sophianielsen221b@gmail.com>
2016-12-02 18:02:53 -0500

    dlib stuff

--- scripts/sight/dlib_ex.py
+++ scripts/sight/dlib_ex.py
@@ -50,15 +50,8 @@ class FaceDetect:
         except CvBridgeError as e:
             print(e)
 
-    def face_detect(self):
-        if self.frame == None:
-            return
-
-        #converts feed to grayscale
-        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        clahe_image = clahe.apply(gray)
-
+    def get_landmarks(image):
+        #code from http://www.paulvangent.com/2016/08/05/emotion-recognition-using-facial-landmarks/
         #detect faces in the image
         detections = self.detector(clahe_image, 1)
 
@@ -68,9 +61,34 @@ class FaceDetect:
             #Get coordinates
             shape = self.predictor(clahe_image, d)
 
+            xlist = []
+            ylist = []
+
             #For each point, draw a red circle with thickness2 on the original frame
             for i in range(1,68): #There are 68 landmark points on each face
                 cv2.circle(self.frame, (shape.part(i).x, shape.part(i).y), 1, (0,0,255), thickness=2)
+                xlist.append(float(shape.part(i).x))
+                ylist.append(float(shape.part(i).y))
+
+            for x, y in zip(xlist, ylist): #Store all landmarks in one list in the format x1,y1,x2,y2,etc.
+                landmarks.append(x)
+                landmarks.append(y)
+        if len(detections) > 0:
+            return landmarks
+        else: #If no faces are detected, return error message to other function to handle
+            landmarks = "error"
+            return landmarks
+
+    def face_detect(self):
+        if self.frame == None:
+            return
+
+        #converts feed to grayscale
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        clahe_image = clahe.apply(gray)
+
+        landmarks = get_landmarks(clahe_image)
 
         cv2.imshow("image", self.frame) #Display the frame
 
