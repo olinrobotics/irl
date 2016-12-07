db48de457b2ddbcb8fdc7864a82b141e8c3529c2
kzhang8850 <kevin.zhang@students.olin.edu>
2016-12-07 16:26:46 -0500

    code refactored, SingleControl integrated with Presence, needs refining but works

--- scripts/sight/presence_detection.py
+++ scripts/sight/presence_detection.py
@@ -23,17 +23,20 @@ class Coordinates:
     def change_Presence(self):
         self.acknowledged = not self.acknowledged
 
+
 class Presence:
     def __init__(self):
         #subscribing to edwin_bodies, from Kinect
         rospy.init_node('edwin_presence', anonymous = True)
 
         rospy.Subscriber('body', SceneAnalysis, self.presence_callback, queue_size=10)
+        rospy.Subscriber('wave_at_me', Int16, self.wave_callback, queue_size=10)
+
 
         #setting up ROS publishers to Edwin commands
         self.behavior_pub = rospy.Publisher('behaviors_cmd', String, queue_size=10)
         self.arm_pub = rospy.Publisher('arm_cmd', String, queue_size=1)
-        self.arm_pub.publish("data: set_speed:: 3000")
+        self.arm_pub.publish("data: set_speed:: 4000")
 
         # tf transformations
         self.br = tf.TransformBroadcaster()
@@ -47,7 +50,14 @@ class Presence:
         self.edwiny = 0
         self.edwinz = 0
 
+        #keeps track of whether someone waved a Edwin or not
+        self.waved = False
 
+
+    def wave_callback(self, waves):
+
+        if int(waves.data) == 1:
+            self.waved = True
 
     def presence_callback(self, scene):
 
@@ -63,70 +73,97 @@ class Presence:
                 else:
                     self.peoples[index].set_Coordinates(xpos, ypos, zpos)
 
-
                 # print self.peoples[index].ID, self.peoples[index].X, self.peoples[index].Y, self.peoples[index].Z
+
+
+    def find_new_people(self):
 
         for person in self.peoples:
             if person is not None and person.acknowledged == False:
                 print "I see you!"
-                time.sleep(2)
+                # time.sleep(3)
                 msg = "data: R_nudge"
                 self.behavior_pub.publish(msg)
-                time.sleep(2)
+                time.sleep(3)
                 msg = "data: R_look"
                 self.behavior_pub.publish(msg)
-                time.sleep(2)
+                time.sleep(3)
                 msg = "rotate_hand:: " + str(-1520)
                 self.arm_pub.publish(msg)
-                time.sleep(2)
+                time.sleep(3)
                 msg = "rotate_wrist:: " + str(-800)
                 self.arm_pub.publish(msg)
                 person.acknowledged = True
 
+        if self.waved == True:
+            print "I saw you wave! Hello!"
+            self.behavior_pub.publish(msg)
+            msg = "data: R_nudge"
+            self.waved = False
+            time.sleep(3)
 
         # print "I am paying attention to person", self.attention()
 
 
+    def follow_people(self):
+
+        for person in self.peoples:
+            if person is not None and self.attention() == person.ID:
+                trans = self.kinect_to_edwin_transform(person)
+                if trans is not None:
+                    xcoord, ycoord, zcoord = self.edwin_transform(trans)
+                    print xcoord, ycoord, zcoord
+
+                    if abs(xcoord - self.edwinx) > 400 or abs(ycoord - self.edwiny) > 400 or abs(zcoord - self.edwinz) > 400:
+                        self.edwinx = xcoord
+                        self.edwiny = ycoord
+                        self.edwinz = zcoord
+                        msg = "move_to:: " + str(self.edwinx) + ", " + str(self.edwiny) + ", " + str(self.edwinz) + ", " + str(11)
+                        self.arm_pub.publish(msg)
+                        time.sleep(.5)
+
+
+    def attention(self):
+
+        center_of_attention = 0
+        distance = 5000
         for person in self.peoples:
             if person is not None:
-                 self.br.sendTransform((person.X, person.Y, person.Z),
-                                  tf.transformations.quaternion_from_euler(0, 0, 0),
-                                  rospy.Time.now(),
-                                  "human",
-                                  "kinect")
+                if person.Z < distance:
+                    center_of_attention = person.ID
+                    distance = person.Z
 
-                 try:
-                     (trans,rot) = self.listener.lookupTransform('/world', '/human', rospy.Time(0))
-                    #  print person.ID, trans
-
-                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                     pass
+        if center_of_attention != 0:
+            return center_of_attention
 
 
-                 if self.attention() == person.ID:
-                     xcoord, ycoord, zcoord = self.edwin_transform(trans)
-                     print xcoord, ycoord, zcoord
+    def kinect_to_edwin_transform(self, person):
 
-                     if abs(xcoord - self.edwinx) > 400 or abs(ycoord - self.edwiny) > 400 or abs(zcoord - self.edwinz) > 400:
-                         self.edwinx = xcoord
-                         self.edwiny = ycoord
-                         self.edwinz = zcoord
-                         msg = "move_to:: " + str(self.edwinx) + ", " + str(self.edwiny) + ", " + str(self.edwinz) + ", " + str(11)
-                         self.arm_pub.publish(msg)
+        self.br.sendTransform((person.X, person.Y, person.Z),
+                            tf.transformations.quaternion_from_euler(0, 0, 0),
+                            rospy.Time.now(),
+                            "human",
+                            "kinect")
 
-                         time.sleep(.5)
+        try:
+            (trans,rot) = self.listener.lookupTransform('/world', '/human', rospy.Time(0))
+            return trans
 
-
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return None
 
 
     def kinect_transform(self, x, y, z):
+
         xposition = x - 320
         yposition = 240 - y
         zposition = z
 
         return zposition, xposition, yposition
 
+
     def edwin_transform(self, coordinates):
+
         edwinx = int(5.485 * coordinates[0] - 1689)
         edwiny = int(7.879 * coordinates[1] - 2794)
         edwinz = int(29.45 * coordinates[2] + 5325)
@@ -149,28 +186,18 @@ class Presence:
         return edwinx, edwiny, edwinz
 
 
-
-    def attention(self):
-
-        center_of_attention = 0
-        distance = 5000
-        for person in self.peoples:
-            if person is not None:
-                if person.Z < distance:
-                    center_of_attention = person.ID
-                    distance = person.Z
-
-        if center_of_attention != 0:
-            return center_of_attention
-
-
     def run(self):
+
         print "running"
         r = rospy.Rate(10)
+        time.sleep(2)
         while not rospy.is_shutdown():
+            self.find_new_people()
+            self.follow_people()
+
             r.sleep()
 
 if __name__ == "__main__":
+
     detector = Presence()
-    time.sleep(1)
     detector.run()
