37fdaac24b4d20c0ccf40f41b09ec74b9ac18a8d
srli <sophia.li@students.olin.edu>
2017-02-11 16:13:46 -0500

    skeleton code for simon demo module

--- scripts/SimonSays.py
+++ scripts/SimonSays.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+"""
+This is the demo script for the SimonSays module. It requires theses modules to be running:
+
+rosrun edwin arm_node.py
+rosrun edwin arm_behaviors.py
+rosrun edwin tts_engine.py
+rosrun edwin stt_engine.py
+"""
+
 import rospy
 import cv2
 import cv2.cv as cv
@@ -6,22 +15,27 @@ import numpy as np
 import random
 import time
 import math
-import operator
-import itertools
+from collections import namedtuple
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from edwin.msg import Edwin_Shape
 from cv_bridge import CvBridge, CvBridgeError
 
+EDWIN_NAME = "edwin"
+USER_NAME = "user"
+
 class Game:
-	def __init__(self):
-		self.draw_pub = rospy.Publisher('draw_cmd', Edwin_Shape, queue_size=10)
+	def __init__(self, max_turns = 5):
 		self.arm_pub = rospy.Publisher('arm_cmd', String, queue_size=10)
 		self.behav_pub = rospy.Publisher('behaviors_cmd', String, queue_size=10)
 
 		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("usb_cam/image_raw", Image, self.img_callback)
+
+		self.SimonCommand = namedtuple("SimonCommand", ["simon_ID", "simon_says", "command"])
+
+		self.max_turns = max_turns
 
 	def img_callback(self, data):
 		try:
@@ -29,44 +43,75 @@ class Game:
 		except CvBridgeError as e:
 			print(e)
 
-
 	def field_scan(self):
 		time.sleep(5)
 
 		cv2.imshow("img", self.frame)
 		c = cv2.waitKey(1)
 
+	def issue_simon_cmd(self):
+		"""If Simon is Edwin:
+
+		This function issues a Simon command.
+		It is said outloud, so depends on the tts_engine to be running"""
+		pass
+
+	def check_simon_response(self):
+		"""If Simon is Edwin:
+
+		This function checks to see if the players have followed Edwin's cmd
+		This relies on skeleton tracker to be functional
+		"""
+		pass
+
+
+	def listen_for_simon(self):
+		"""If Simon is the User:
+
+		This function listens to a spoken Simon command from user
+		This relies on stt_engine to be running"""
+		pass
+
+	def follow_simon_cmd(self):
+		"""If Simon is the User:
+
+		This function takes the command from listen_for_simon() and interprets
+		The resulting command is sent to arm_node for physical motion"""
+		pass
+
+
+
 	def run(self):
+		"""Game mainloop. Runs for as long as max_turns is defined"""
+
 	 	time.sleep(5)
 	 	print "running"
 
-		self.arm_pub.publish("data: set_speed:: 1000")
-		print "set speed to 1000"
-		time.sleep(1)
+		self.simon_ID = EDWIN_NAME
+		autonomous_play = False
 
-		self.arm_pub.publish("data: set_accel:: 100")
-		print "set accel to 100"
-		time.sleep(1)
+		#two phases of simon says, issue command, check for follower response
+		turn_count = 0
+		while turn_count < self.max_turns:
+			turn_count += 1
 
+			#if playing autonomously, then Edwin issues commands and follows his own
+			if autonomous_play:
+				self.issue_simon_cmd()
+				self.follow_simon_cmd()
+				continue
 
-		#Player = 0
-        #Edwin  = 1
+			#issue command
+			if self.simon_ID == EDWIN_NAME:
+				self.issue_simon_cmd()
+			else:
+				self.listen_for_simon()
 
-        running = True
-		# turn = random.randint(0,1)
-		turn = 0
-		if turn == 0:
-			print "YOUR TURN"
-			self.behav_pub.publish("nudge")
-			time.sleep(2)
-
-		ai = False
-		while running:
- 	 		if turn == 0: #Player turn.
-	 	         print "player's turn"
-
-	 		elif turn == 1: #Edwin's turn
-	 			print "edwin's turn"
+			#check for response
+			if self.simon_ID == EDWIN_NAME:
+				self.check_simon_response()
+			else:
+				self.follow_simon_cmd()
 
 		cv2.destroyAllWindows()
 		print "Finished with Simon Says, hope you enjoyed :)"
