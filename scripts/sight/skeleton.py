d9b1c7b993b19418a30d436b4513520c525cf087
kzhang8850 <kevin.zhang@students.olin.edu>
2017-02-25 18:36:34 -0500

    skeleton tracking is essentially done

--- scripts/sight/skeleton.py
+++ scripts/sight/skeleton.py
@@ -5,32 +5,47 @@ import numpy as np
 from std_msgs.msg import String, Int16
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
-from edwin.msg import *
+from edwin.msg import Bones, HHH
 import time
 import tf
 import cv2
 from visualization_msgs.msg import Marker
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class Skeleton(object):
     """
     Class to hold and use skeleton of users
     """
     def __init__(self):
+        #init ROS node
         rospy.init_node("skeleton", anonymous=True)
-        rospy.Subscriber("/skeleton_markers", Marker, self.constructSkeleton, queue_size=10)
-        rospy.Subscriber("/camera/rgb/image_raw", Image, self.renderImage, queue_size=10)
 
-        # self.skelepub = rospy.Publisher("/skeleton", , queue_size=10)
-
-        self.listener = tf.TransformListener()
-
-
+        #image stuff
         self.bridge = CvBridge()
         self.cv_image = None
+
+        #skeleton stuff
         self.body_points = None
         self.head = (0,0,0)
+        self.torso = (0,0,0)
+        self.Rhand = (0,0,0)
+        self.Lhand = (0,0,0)
+
+        #running
         self.running = True
+
+
+        #subscribing to skeleton markers
+        rospy.Subscriber("/skeleton_markers", Marker, self.constructSkeleton, queue_size=10)
+
+        #subscribing to Kinect camera
+        rospy.Subscriber("/camera/rgb/image_raw", Image, self.renderImage, queue_size=10)
+
+        #makes two publishers, one for the head and hands, and one for the entire body
+        self.presencePub = rospy.Publisher("/presence", HHH, queue_size=10)
+        self.skelePub = rospy.Publisher("/skeleton", Bones, queue_size=10)
+
 
 
     def constructSkeleton(self, skeleton):
@@ -48,46 +63,101 @@ class Skeleton(object):
         4 - right elbow
         5 - right hand
 
-        6 - left shoulder
-        7 - left elbow
-        8 - left hand
+        6 - right hip
+        7 - right knee
+        8 - right foot
 
-        9 - right hip
-        10 - right knee
-        11 - right foot
+        9 - left shoulder
+        10 - left elbow
+        11- left hand
 
         12 - left hip
         13 - left knee
         14 - left foot
         """
+        #gets the skeleton and extracts the head and hands
         self.body_points = skeleton.points
-        temp = self.body_points[0]
-        print "raw", temp
-        self.head = self.transform_skel2kinect(temp)
-        print "processed", self.head
-        print " "
+        raw_head = self.body_points[0]
+        raw_torso = self.body_points[2]
+        raw_Rhand = self.body_points[5]
+        raw_Lhand = self.body_points[11]
+
+        #converts the head and hands to Kinect coordinate frame
+        self.head = self.transform_skel2kinect(raw_head)
+        self.torso = self.transform_skel2kinect(raw_torso)
+        self.Rhand = self.transform_skel2kinect(raw_Rhand)
+        self.Lhand = self.transform_skel2kinect(raw_Lhand)
+
+        print "processed", type(self.body_points[0])
 
 
     def renderImage(self, image):
         """
         Renders an image using opencv
         """
+        #extracts the image
         self.cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+
+        #converts head coordinates to image coordinates for visualization
         imagex = 320 - int(self.head[0])
         imagey = 240 - int(self.head[1])
         cv2.circle(self.cv_image, (imagex, imagey), 10, 255, thickness = -1)
 
+        #converts hand coordinates to image coordinates for visualization
+        imagex = 320 - int(self.Rhand[0])
+        imagey = 240 - int(self.Rhand[1])
+        cv2.circle(self.cv_image, (imagex, imagey), 10, 255, thickness = -1)
+        imagex = 320 - int(self.Lhand[0])
+        imagey = 240 - int(self.Lhand[1])
+        cv2.circle(self.cv_image, (imagex, imagey), 10, 255, thickness = -1)
 
-    def transform_skel2kinect(self, temp):
+
+
+    def transform_skel2kinect(self, bodypart):
         """
         makes a transformation from the coordinates of the skeleton to the coordinates
         of the Kinect image
         """
-
-        x = temp.y/temp.x * 640
-        y = temp.z/temp.x * 480
-        z = temp.x
+        #converts based on triangular ratio from a camera FOV
+        x = bodypart.y/bodypart.x * 640
+        y = bodypart.z/bodypart.x * 480
+        z = bodypart.x
         return (x, y, z)
+
+    def send_out_data(self):
+        """
+        publishes out data to both presence detection topic and simon says topic
+        """
+        #make the messages
+        bones = Bones()
+        presence = HHH()
+
+        #fill them with data
+        if self.body_points is not None:
+            bones.h = self.body_points[0]
+            bones.n = self.body_points[1]
+            bones.t = self.body_points[2]
+            bones.rs = self.body_points[3]
+            bones.re = self.body_points[4]
+            bones.rh = self.body_points[5]
+            bones.rp = self.body_points[6]
+            bones.rk = self.body_points[7]
+            bones.rf = self.body_points[8]
+            bones.ls = self.body_points[9]
+            bones.le = self.body_points[10]
+            bones.lh = self.body_points[11]
+            bones.lp = self.body_points[12]
+            bones.lk = self.body_points[13]
+            bones.lf = self.body_points[14]
+
+            self.skelePub.publish(bones)
+
+        presence.head = self.head
+        presence.torso = self.torso
+        presence.rhand = self.Rhand
+        presence.lhand = self.Lhand
+
+        self.presencePub.publish(presence)
 
 
 
@@ -97,7 +167,7 @@ class Skeleton(object):
         """
         print "running presence skeleton"
         r = rospy.Rate(10)
-        time.sleep(1)
+        time.sleep(2)
 
         cv2.namedWindow("chicken")
 
@@ -107,6 +177,8 @@ class Skeleton(object):
                 cv2.waitKey(3)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+
+            self.send_out_data()
 
             r.sleep()
 
