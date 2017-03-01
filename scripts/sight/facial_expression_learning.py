b028ef0f41f3387d099c55db6bf559c3d6f78112
snielsen221b <sophianielsen221b@gmail.com>
2017-03-01 18:00:29 -0500

    workign on k means

--- scripts/sight/facial_expression_learning.py
+++ scripts/sight/facial_expression_learning.py
@@ -1,37 +1,8 @@
+import rospkg
+import rospy
+
 from skimage import data
 import numpy as np
 
-# grab some sample data
-camera = data.camera()
-
-# MASKING
-# create mask for camera with values greater than 87 masked
-mask = camera > 123
-# set mask to white (255) pixels where mask is True
-camera[mask] = 255
-# makes an array the same size of the image
-nrows, ncols = camera.shape
-row, col = np.ogrid[:nrows, :ncols]
-# can also use boolean artihmetic for more complex masks (see np.logical_and)
-
-
-# FANCY INDEXING
-# values from 0 to number of rows
-ind_r = np.arange(len(camera))
-# every fourth column (this workds b/c rows = columns)
-ind_c = 4 * ind_r % len(camera)
-camera[ind_r, ind_c] = 0
-
-# COLOR IMAGES
-# color images will have a depth of 3
-cat = data.chelsea()
-print(cat.shape)
-
-# using masks in color images
-reddish = cat[:, :, 0] > 160
-cat[reddish] = [0, 255, 0]
-
-# look for coordinate conventions and array order notes:
-# http://scikit-image.org/docs/dev/user_guide/numpy_images.html
 
 # TODO: load an image as a numpy array
--- scripts/sight/k-means.py
+++ scripts/sight/k-means.py
@@ -1,42 +1,15 @@
-# code from http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_assumptions.html#sphx-glr-auto-examples-cluster-plot-kmeans-assumptions-py
-
+# ref http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_assumptions.html#sphx-glr-auto-examples-cluster-plot-kmeans-assumptions-py
+# http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html
+from sklearn import metrics
 from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
+from sklearn.datasets import PCA
+from sklean.decompoisition import scale
 
-import matplotlib.pyplot as plt
-import numpy as np
 
-plt.figure(figsize=(12, 12))
+class Cluster:
+    def __init__(self, data=scale(load_digits().data)):
+        self.data = data
+        self.n_samples, self.n_features = self.data.shape
 
-n_samples = 1500
-random_state = 170
-X, y = make_blobs(n_samples=n_samples, random_state=random_state)
 
-# Incorrect number of clusters
-y_pred = KMeans(n_clusters=2, random_state=random_state)
-
-# Makes a scatter plot of x vs. y
-plt.subplot(221)
-plt.scatter(X[:, 0], X[:, 1],  c=y_pred)
-plt.title("Incorrect Number of Blobs")
-
-# Anisotropicly distributed data (not distributed symetrically)
-transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
-
-# X = np.array([[1, 1], [1, 2], [2, 1], [2, 2], [5, 5], [5, 6], [6, 5], [6, 6],
-              # [7, 1], [7, 2], [8, 1], [8, 2]])
-# .fix computes k-means clustering
-# '''kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
-# estimator = KMeans(n_clusters=3)
-
-# samples = 12
-# features = 3
-# labels = estimator.labels_
-# # kmeans.predict(X)
-
-# # Ploting figure
-# plt.clf
-# fig = plt.figure(figsize=(4, 3))
-# plt.cla
-# plt.plot(X)
-# plt.show()'''
+if __name__ == "__main__":
