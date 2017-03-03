e9eda8229b06f13896c4e538beaeba25b9b4c0fb
hkolano <hannah.kolano@students.olin.edu>
2017-03-03 18:11:43 -0500

    now makes right tree! yay!

--- scripts/sight/math_interp.py
+++ scripts/sight/math_interp.py
@@ -9,7 +9,7 @@ MAKE SURE ROSCORE IS RUNNING
 RUN IT AS $rosrun edwin math_interp.py
 NEXT STEP:
 take out cases of double operatives, etc.
-doesn't make the right OofOp tree :
+figure out NEGATIVES and figure out how to change 2x to 2*x
 '''
 from __future__ import division
 import rospy
@@ -18,12 +18,12 @@ from std_msgs.msg import String
 
 
 class Calculator:
-
     def __init__(self):
         '''initializes the object'''
         rospy.init_node('doing_math')
         # self.pub = rospy.Publisher('/math_output', String, queue_size=10)
-        self.eqn = ''
+        self.eqn = Equation()
+        self.tree = tuple()
         # rospy.Subscriber('word_publish', String, self.cmd_callback)
 
         self.integer_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
@@ -38,20 +38,6 @@ class Calculator:
         '''callback'''
         given_string = str(data)
         self.eqn = given_string[6:]
-
-    def makes_sense(self, eqn):
-        '''if data is something it can solve, send it to simple_equation.
-        else, print why it can't solve it.'''
-
-        # self.split_into_list = list(data)
-        # for element in self.split_into_list:
-        #     if element not in self.basics_list:
-        #         if element in self.basics_and_variables:
-        #             return self.algebra_solver(data)
-        #         else:
-        #             print('I do not know how to do that yet :(')
-        #             return
-        # return self.simple_equation(data)
 
     def solve_simple(self, eqn):
         '''solves a simple expression'''
@@ -91,50 +77,58 @@ class Calculator:
         operation present, keep processsing and return the processed
         side. If not, return itself.'''
         for element in side:
-            if element not in self.integer_list and element not in self.variable_list:
+            if element in self.operator_list:
                 return self.build_tree(side)
         return side
 
     def build_tree(self, side):
         '''take in a side of an equation, create a tree with the
         operations as nodes, returns that tree.'''
-        self.tree = tuple()
+        # print(side)
         if '/' in side or '*' in side:
             indexdiv = side.find('/')
-            if indexdiv == -1:
-                indexdiv = 10
             indexmul = side.find('*')
-            if indexmul == -1:
-                indexmul = 10
-
-            if indexmul < indexdiv:
+            if indexdiv == -1:
                 index = indexmul
                 element = '*'
-            elif indexdiv < indexmul:
+            elif indexmul == -1:
                 index = indexdiv
                 element = '/'
+            else:
+                if indexmul < indexdiv:
+                    index = indexmul
+                    element = '*'
+                else:
+                    index = indexdiv
+                    element = '/'
             left_ele = side[:index]
             right_ele = side[index+1:]
             self.tree = (element, self.tree_base_case_check(left_ele), self.tree_base_case_check(right_ele))
 
         if '-' in side or '+' in side:
-            indexmin = side.find('-')
             indexplus = side.find('+')
-            if indexplus < indexmin and indexplus > -1:
-                index = indexplus
-                element = '+'
-            elif indexmin < indexplus and indexmin > -1:
+            indexmin = side.find('-')
+            if indexplus == -1:
                 index = indexmin
                 element = '-'
+            elif indexmin == -1:
+                index = indexplus
+                element = '+'
+            else:
+                if indexmin < indexplus:
+                    index = indexmin
+                    element = '-'
+                else:
+                    index = indexplus
+                    element = '+'
             left_ele = side[:index]
             right_ele = side[index+1:]
             self.tree = (element, self.tree_base_case_check(left_ele), self.tree_base_case_check(right_ele))
         return self.tree
 
-    def solve_tree(self, rightside, leftside):
-        for item in rightside:
-            if item in self.variable_list:
-                pass
+    def unpack_tree(self, rightside, leftside):
+        '''takes a tree tuple and changes it back to a string'''
+        pass
 
     def check_triviality(self, answer):
         '''returns 1 if getting a value from the subscriber;
@@ -152,11 +146,29 @@ class Calculator:
         if self.check_triviality(self.eqn) == 1:
             prev_answer = answer
             answer = self.solve_algebra(self.eqn)
-            if answer != prev_answer:
-                print(answer)
+            # if answer != prev_answer:
+            #     print(answer)
+
+
+class Equation(Calculator):
+    def __init__(self):
+        self.left_tuple = tuple()
+        self.right_tuple = tuple()
+        self.left_string = ''
+        self.right_string = ''
+
+    def __str__(self):
+        return '%d = %d' % (self.left_string, self.right_string)
+
+    def string_to_tree(self, side):
+        side_tree = self.tree_base_case_check(side)
+        return side_tree
+
+    def tree_to_string(self, tree):
+        pass
 
 
 if __name__ == '__main__':
     ctr = Calculator()
-    ctr.eqn = '2*x/3*4=3'
+    ctr.eqn = '5*x/2+3=5'
     ctr.run()
