6e5e47a5a07bf9a2281958f6c2f7d96c622ac364
snielsen221b <sophianielsen221b@gmail.com>
2017-03-03 18:04:06 -0500

    k-means, attributes defined

--- scripts/sight/k-means.py
+++ scripts/sight/k-means.py
@@ -6,10 +6,20 @@ from sklearn.datasets import PCA
 from sklean.decompoisition import scale
 
 
+# Default data_set from sklean.datasets: http://scikit-learn.org/stable/modules/classes.html#module-sklearn.datasets
+# The data_set is a tuple (X, y) where X = an n_samples by n_features numpy array
+# and an array of length n_samples
 class Cluster:
-    def __init__(self, data=scale(load_digits().data)):
-        self.data = data
+    def __init__(self, data_set=load_digits()):
+        self.data_set = data_set
+        self.data = scale(self.data_set.data)
         self.n_samples, self.n_features = self.data.shape
+        self.n_clusters = len(np.unique(digits.target))
+        self.sample_size = 300
 
+    def bench_k_means(estimator, name, data):
+        t0 = time()
+        estimator.fit(self.data)
+        
 
 if __name__ == "__main__":
