2458deafbbccbc77d05dda000741344ca51b25df
hkolano <hannah.kolano@students.olin.edu>
2017-03-10 19:01:25 -0500

    IT DOES ALGEBRAgit add math_interp.py ! :D

--- scripts/sight/math_interp.py
+++ scripts/sight/math_interp.py
@@ -7,9 +7,11 @@ hannah.kolano@studets.olin.edu
 HANNAH
 MAKE SURE ROSCORE IS RUNNING
 RUN IT AS $rosrun edwin math_interp.py
+
 NEXT STEP:
-take out cases of double operatives, etc.
-figure out NEGATIVES and figure out how to change 2x to 2*x
+figure out NEGATIVES
+figure out how to change 2x to 2*x
+solve the 1/x/2 problem
 
 '''
 from __future__ import division
@@ -29,7 +31,6 @@ class Calculator:
         # rospy.Subscriber('word_publish', String, self.cmd_callback)
 
         self.integer_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
-        self.order_of_ops = [('*', '/'), ('+', '-')]
         self.operator_list = ['+', '-', '/', '*', '=']
         self.variable_list = ['x', 'y', 'z']
         self.opposite_operation = {'+': '-', '-': '+', '/': '*', '*': '/'}
@@ -79,9 +80,9 @@ class Calculator:
 
     def tree_base_case_check(self, side):
         '''takes in one side of the equation. if there's still an
-        operation present, keep processsing and return the processed
+        operation present, keep processsing and return the processed (tree'd)
         side. If not, return itself.'''
-        for element in side:
+        for element in str(side):
             if element in self.operator_list:
                 return self.build_tree(side)
         return side
@@ -93,8 +94,8 @@ class Calculator:
         if self.variable not in side:
             return str(eval(side))
         if '/' in side or '*' in side:
-            indexdiv = side.find('/')
-            indexmul = side.find('*')
+            indexdiv = side.rfind('/')
+            indexmul = side.rfind('*')
             if indexmul > indexdiv:
                 index = indexmul
                 element = '*'
@@ -106,8 +107,8 @@ class Calculator:
             self.tree = (element, self.tree_base_case_check(left_ele), self.tree_base_case_check(right_ele))
 
         if '-' in side or '+' in side:
-            indexplus = side.find('+')
-            indexmin = side.find('-')
+            indexplus = side.rfind('+')
+            indexmin = side.rfind('-')
             if indexmin > indexplus:
                 index = indexmin
                 element = '-'
@@ -136,81 +137,55 @@ class Calculator:
         return equation_string
 
     def solve_algebra(self):
+        '''takes the self.trees and self.strings and does the next appropriate
+        operation to them'''
         if self.side_w_variable == 'left':
-            if type(self.lstree[1]) == str:
-                mov_idx = 1
-                keep_idx = 2
-            elif type(self.lstree[2]) == str:
-                mov_idx = 2
-                keep_idx = 1
+            vt_vs_nvt_nvs = [self.lstree, self.lsstring, self.rstree, self.rsstring]
+        elif self.side_w_variable == 'right':
+            vt_vs_nvt_nvs = [self.rstree, self.rsstring, self.lstree, self.lsstring]
 
-            if self.lstree[0] == '+':
-                var_side, non_var_str, var_side_str, non_var_tree = self.plus_operation(self.lstree, self.rsstring, self.lsstring, self.rstree, mov_idx, keep_idx)
-            elif self.lstree[0] == '-':
-                var_side, non_var_str, var_side_str, non_var_tree = self.minus_operation(self.lstree, self.rsstring, self.lsstring, self.rstree)
-            elif self.lstree[0] == '*':
-                var_side, non_var_str, var_side_str, non_var_tree = self.multiply_operation(self.lstree, self.rsstring, self.lsstring, self.rstree, mov_idx, keep_idx)
-            elif self.lstree[0] == '/':
-                var_side, non_var_str, var_side_str, non_var_tree = self.divide_operation(self.lstree, self.rsstring, self.lsstring, self.rstree)
+        if type(vt_vs_nvt_nvs[0][1]) == str:
+            mov_idx = 1
+            keep_idx = 2
+        elif type(vt_vs_nvt_nvs[0][2]) == str:
+            mov_idx = 2
+            keep_idx = 1
 
-            self.lstree = var_side
-            self.rsstring = non_var_str
-            self.lsstring = var_side_str
-            self.rstree = non_var_tree
+        if vt_vs_nvt_nvs[0][0] == '+':
+            vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3] = self.do_op(vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3], '-', mov_idx, keep_idx)
+        elif vt_vs_nvt_nvs[0][0] == '-':
+            vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3] = self.do_op(vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3], '+')
+        elif vt_vs_nvt_nvs[0][0] == '*':
+            vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3] = self.do_op(vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3], '/', mov_idx, keep_idx)
+        elif vt_vs_nvt_nvs[0][0] == '/':
+            vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3] = self.do_op(vt_vs_nvt_nvs[0], vt_vs_nvt_nvs[3], '*')
 
-        if self.side_w_variable == 'right':
-            if type(self.rstree[1]) == str:
-                mov_idx = 1
-                keep_idx = 2
-            elif type(self.rstree[2]) == str:
-                mov_idx = 2
-                keep_idx = 1
+        vt_vs_nvt_nvs[1] = self.tree_to_string(vt_vs_nvt_nvs[0])
+        vt_vs_nvt_nvs[2] = self.tree_base_case_check(vt_vs_nvt_nvs[3])
 
-            if self.lstree[0] == '+':
-                var_side, non_var_str, var_side_str, non_var_tree = self.plus_operation(self.rstree, self.lsstring, self.rsstring, self.lstree, mov_idx, keep_idx)
-            elif self.lstree[0] == '-':
-                var_side, non_var_str, var_side_str, non_var_tree = self.minus_operation(self.rstree, self.lsstring, self.rsstring, self.lstree)
-            elif self.lstree[0] == '*':
-                var_side, non_var_str, var_side_str, non_var_tree = self.multiply_operation(self.rstree, self.lsstring, self.rsstring, self.lstree, mov_idx, keep_idx)
-            elif self.lstree[0] == '/':
-                var_side, non_var_str, var_side_str, non_var_tree = self.divide_operation(self.rstree, self.lsstring, self.rsstring, self.lstree)
+        if self.side_w_variable == 'left':
+            self.lstree, self.lsstring, self.rstree, self.rsstring = vt_vs_nvt_nvs
+        elif self.side_w_variable == 'right':
+            self.rstree, self.rsstring, self.lstree, self.lsstring = vt_vs_nvt_nvs
 
-            self.rstree = var_side
-            self.lsstring = non_var_str
-            self.rsstring = var_side_str
-            self.lstree = non_var_tree
+        if self.variable in str(self.lsstring):
+            self.side_w_variable = 'left'
+        elif self.variable in str(self.rsstring):
+            self.side_w_variable = 'right'
+
         # print(self.lstree)
         # print(self.lsstring)
         # print(self.rstree)
         # print(self.rsstring)
 
-    def plus_operation(self, var_side_tree, non_var_str, var_side_str, non_var_tree, mov_idx, keep_idx):
-        non_var_str = eval(str(non_var_str) + '-' + str(var_side_tree[mov_idx]))
+    def do_op(self, var_side_tree, non_var_str, string, mov_idx=2, keep_idx=1):
+        '''given a tree and the opposite side string, snips the operation
+        from the tree and moves it to the other side string.'''
+        non_var_str = str(non_var_str) + string + str(var_side_tree[mov_idx])
+        if self.variable not in non_var_str:
+            non_var_str = eval(non_var_str)
         var_side_tree = var_side_tree[keep_idx]
-        var_side_str = self.tree_to_string(var_side_tree)
-        non_var_tree = self.tree_base_case_check(non_var_tree)
-        return var_side_tree, non_var_str, var_side_str, non_var_tree
-
-    def minus_operation(self, var_side_tree, non_var_str, var_side_str, non_var_tree):
-        non_var_str = eval(str(non_var_str) + '+' + str(var_side_tree[2]))
-        var_side_tree = var_side_tree[1]
-        var_side_str = self.tree_to_string(var_side_tree)
-        non_var_tree = self.tree_base_case_check(non_var_tree)
-        return var_side_tree, non_var_str, var_side_str, non_var_tree
-
-    def multiply_operation(self, var_side_tree, non_var_str, var_side_str, non_var_tree, mov_idx, keep_idx):
-        non_var_str = eval(str(non_var_str) + '/' + str(var_side_tree[mov_idx]))
-        var_side_tree = var_side_tree[keep_idx]
-        var_side_str = self.tree_to_string(var_side_tree)
-        non_var_tree = self.tree_base_case_check(non_var_tree)
-        return var_side_tree, non_var_str, var_side_str, non_var_tree
-
-    def divide_operation(self, var_side_tree, non_var_str, var_side_str, non_var_tree):
-        non_var_str = eval(str(non_var_str) + '*' + str(var_side_tree[2]))
-        var_side_tree = var_side_tree[1]
-        var_side_str = self.tree_to_string(var_side_tree)
-        non_var_tree = self.tree_base_case_check(non_var_tree)
-        return var_side_tree, non_var_str, var_side_str, non_var_tree
+        return var_side_tree, non_var_str
 
     def check_triviality(self, answer):
         '''returns 1 if getting a value from the subscriber;
@@ -227,10 +202,10 @@ class Calculator:
         # while not rospy.is_shutdown():
         if self.check_triviality(self.eqn) == 1:
             self.initialize_algebra(self.eqn)
-            print(self.lstree)
-            print(self.lsstring)
-            print(self.rstree)
-            print(self.rsstring)
+            # print(self.lstree)
+            # print(self.lsstring)
+            # print(self.rstree)
+            # print(self.rsstring)
             # prev_answer = answer
             while self.rstree != self.variable and self.lsstring != self.variable:
                 self.solve_algebra()
@@ -241,5 +216,5 @@ class Calculator:
 
 if __name__ == '__main__':
     ctr = Calculator()
-    ctr.eqn = '2*x+4/2-2=4'
+    ctr.eqn = '3*6*y=18'
     ctr.run()
