8eb01e14b8fa4bc1d0bbbac3d66057ffaa181286
snielsen221b <sophianielsen221b@gmail.com>
2017-03-25 17:34:10 -0400

    saving progress

--- scripts/sight/get_features_face.py
+++ scripts/sight/get_features_face.py
@@ -11,13 +11,14 @@ class Features:
         # defines file paths
         rospack = rospkg.RosPack()
         PACKAGE_PATH = rospack.get_path("edwin")
-        self.faces_folder_path = PACKAGE_PATH + '/params/cohn-kanade-images'
+        self.faces_folder_path = PACKAGE_PATH + \
+            '/params/database/cohn-kanade-images'
         self.emotion_labels_folder_path = PACKAGE_PATH + '/params/Emotion'
 
         self.face_detect = dl.FaceDetect()
 
     def get_clahe_image(self, f):
-        im = cv2.IMREAD_GRAYSCALE(f)
+        im = cv2.imread(f)
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
         clahe_image = clahe.apply(gray)
