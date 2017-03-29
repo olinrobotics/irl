a102145d4acd28adb3b09bc0c45831b1d4a8ce02
Katya <katya@computer.com>
2017-03-29 17:32:34 -0400

    commiting changes

--- scripts/edwin_characterization.py
+++ scripts/edwin_characterization.py
@@ -7,26 +7,37 @@ import time
 import csv
 from std_msgs.msg import String
 from edwin.msg import Bones
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import random
 
 #with open('file.csv', newline='') as csvfile:
 #     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 #     for row in spamreader:
 #         print(', '.join(row))
+#def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+"""def E_distance(data1, data2, len_data):
+    distance = 0
+    for x in data1:
+        if x > 1:
+            distance += (data1[x]-data2[x])**2
+    return math.sqrt(distance)
+"""
 
 
-with open('file.csv', 'rU') as file:
-    table = [row for row in csv.reader(file)]
-print(table)
 
-with open('skeleton.csv', 'rU') as file:
+"""with open('skeleton.csv', 'rU') as file:
     label= [col['A'] for col in csv.reader(file)]
-    training_data = [col['E':'AE'] for col in csv.reader(file)]
+    training_data = [col['E':'AE'] for col in csv.reader(file)
+list_of_files = ['l_hand_head','l_hand_r_hand','l_hand_r_shoulder','l_hand_stomach',
+                 'r_hand_head','r_hand_l_shoulder','r_hand_stomach']
+
+df = pd.read_csv(skeleton.csv)
+training_data= df['E':'AE']
+print(df['E':'AE'])
+"""
 
 
-
-neighbors = KNeighborsClassifier(n_neighbors=7,algorithm='ball_tree')
-neighbors.fit( training_data, 'labels')
 """list_of_files = ['l_hand_head','l_hand_r_hand','l_hand_r_shoulder','l_hand_stomach',
                  'r_hand_head','r_hand_l_shoulder','r_hand_stomach']
 for i in list_of_files:
@@ -39,92 +50,80 @@ for i in list_of_files:
 
 
 class Gestures:
-
     def __init__(self,skeleton):
         self.skeleton = skeleton
         rospy.init_node('gestures', anonymous=True)
-        rospy.Subscriber('/skeleton', String, self.data_callback)
-        self.pub = rospy.Publisher('skeleton_points', Bones, queue_size=10)
+        rospy.Subscriber('/skeleton', Bones, self.data_callback)
+        self.pub = rospy.Publisher('skeleton_points', String, queue_size=10)
+        self.trainingSet = []
+        self.testSet = []
+        self.labels_test =[]
+        self.labels_train = []
+        self.data_from_sub = []
+        with open('skeleton.csv', 'r') as csvfile:
+            lines = csv.reader(csvfile)
+            labels =[]
+            dataset =[]
+            for rows in lines:
+                labels.append(rows[0])
+                dataset.append(rows[1:])
+            dataset= dataset[1:]
+            #labels= labels[1:]
+            #print(dataset)
+            for i in dataset:
+                for l in range(len(i)):
+                    if l % 3 == 0:
+                        i[l]= float(i[l])+float(i[3])
+                    if l % 3 == 1:
+                        i[l]= float(i[l])+float(i[4])
+                    if l % 3 == 2:
+                        i[l]= float(i[l])+float(i[5])
+            #print(labels)
+            #print(dataset)
+            for x in range(len(dataset)-1):
+                if random.random() < .8 :
+                    self.trainingSet.append(dataset[x])
+                    self.labels_train.append(labels[x])
+                else:
+                    self.testSet.append(dataset[x])
+                    self.labels_test.append(labels[x])
+        self.neighbors= KNeighborsClassifier()
         time.sleep(2)
-        #pub = rospy.Publisher('something', std_msgs.msg.String, queue_size=10)
         #recieving data in forms of a list of tuples
 
     def data_callback(self,data):
-        data.h.x
-        data.h.y
-        data.h.z
-        data.n.x
-        data.n.y
-        data.n.z
-        data.t.x
-        data.t.y
-        data.t.z
-        data.rs.x
-        data.rs.y
-        data.rs.z
-        data.re.x
-        data.re.y
-        data.re.z
-        data.rh.x
-        data.rh.y
-        data.rh.z
-        data.ls.x
-        data.ls.y
-        data.ls.z
-        data.le.x
-        data.le.y
-        data.le.z
-        data.lh.x
-        data.lh.y
-        data.lh.z
-        data.rp.x
-        data.rp.y
-        data.rp.z
-        data.rk.x
-        data.rk.y
-        data.rk.z
-        data.rf.x
-        data.rf.y
-        data.rf.z
-        data.lp.x
-        data.lp.y
-        data.lp.z
-        data.lk.x
-        data.lk.y
-        data.lf.y
-        data.lk.z
-        data.lf.x
-        data.lf.z
-        print(data.data)
-        self.pub.publish("Gesture detection is running")
+        self.data_from_sub = []
+        self.data_from_sub.extend([data.h.x,data.h.y,data.h.z])
+        self.data_from_sub.extend([data.n.x,data.n.y,data.n.z])
+        self.data_from_sub.extend([data.t.x,data.t.y,data.t.z])
+        self.data_from_sub.extend([data.rs.x,data.rs.y,data.rs.z])
+        self.data_from_sub.extend([data.re.x,data.re.y,data.re.z])
+        self.data_from_sub.extend([data.rh.x,data.rh.y,data.rh.z])
+        self.data_from_sub.extend([data.ls.x,data.ls.y,data.ls.z])
+        self.data_from_sub.extend([data.le.x,data.le.y,data.le.z])
+        self.data_from_sub.extend([data.lh.x,data.lh.y,data.lh.z])
+        for l in range(len(self.data_from_sub)):
+            if l % 3 == 0:
+                self.data_from_sub[l]= float(self.data_from_sub[l])+float(self.data_from_sub[3])
+            if l % 3 == 1:
+                self.data_from_sub[l]= float(self.data_from_sub[l])+float(self.data_from_sub[4])
+            if l % 3 == 2:
+                self.data_from_sub[l]= float(self.data_from_sub[l])+float(self.data_from_sub[5])
 
-    def body_parts(self):
-        head = self.skeleton[0]
-        left_hand = self.skeleton[1]
-        right_hand = self.skeleton[2]
-        left_shoulder = self.skeleton[3]
-        right_shoulder = self.skeleton[4]
-        left_elbow = self.skeleton[5]
-        right_elbow = self.skeleton[6]
-        chest = self.skeleton[7]
-
-    def close_points(self):
-        distance_x =abs(self.skeleton[0][0]-self.skeleton[1][0])
-        distance_y = abs(self.skeleton[0][1]-self.skeleton[1][1])
-        distance_btwn_xy= math.sqrt((distance_x)**2 + (distance_y)**2)
-        distance_z = abs(self.skeleton[0][2]-self.skeleton[1][2])
-        distance_btwn_points= math.sqrt((distance_btwn_xy)**2+(distance_z)**2)
-        if distance_btwn_points <5:
-            self.pub.publish("True")
-            return True
-        else:
-            self.pub.publish("False")
-            return False
+    def machine_learning(self):
+        self.neighbors = KNeighborsClassifier(n_neighbors=7)
+        self.neighbors.fit( self.trainingSet, self.labels_train)
+        #print(self.data_from_sub)
+        self.pub.publish(self.neighbors.predict([self.data_from_sub])[0])
+        #print(self.neighbors.predict([self.data_from_sub]))
+        #print(self.neighbors.score(self.testSet, self.labels_test))
 
     def run(self):
         r = rospy.Rate(10)
+        self.machine_learning()
         while not rospy.is_shutdown():
             # self.close_points()
+            self.machine_learning()
             r.sleep()
 
 if __name__ == "__main__":
