70a93fe1e8d3a2a2ecfee9d784a086d36c1e3c6f
Jamie Cho <jchocholate@gmail.com>
2017-09-23 19:56:44 -0400

    now part of repo

---
+++ edwin_moveit_config/include/edwin_interface.h
@@ -0,0 +1,49 @@
+#ifndef __EDWIN_INTERFACE_H__
+#define __EDWIN_INTERFACE_H__
+
+#include <ros/ros.h>
+
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+
+#include <std_msgs/String.h>
+#include <sensor_msgs/JointState.h>
+
+#include <string>
+
+#include "st_arm.h"
+
+
+class EdwinInterface: public hardware_interface::RobotHW{
+	private:
+		STArm st;
+
+		hardware_interface::JointStateInterface jnt_state_interface;
+		hardware_interface::PositionJointInterface jnt_pos_interface;
+
+		double cmd[N_JOINTS];
+		double pos[N_JOINTS];
+		double vel[N_JOINTS];
+		double eff[N_JOINTS];
+
+		ros::NodeHandle nh;
+
+		ros::Publisher pub;
+		ros::Subscriber sub;
+
+		std_msgs::String cmd_msg;
+
+		sensor_msgs::JointState joint_state_msg;
+	public:
+		EdwinInterface(ros::NodeHandle nh, const std::string&);
+		ros::Time get_time();
+		void arm_cmd_cb(const std_msgs::StringConstPtr& msg);
+		virtual void read(const ros::Time& time);
+		virtual void write(const ros::Time& time);
+};
+
+enum {WRIST,HAND,ELBOW,SHOULDER,WAIST};
+extern const std::string joints[];// = {"wrist","hand","elbow","shoulder","waist"};
+
+#endif
