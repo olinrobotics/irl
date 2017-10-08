d4516a0e1d4e53be82c3ee0a53a1954a264c4c0b
hkolano <hannah.kolano@students.olin.edu>
2017-10-07 20:19:01 -0400

    just commit already

---
+++ dino_arms/projects/connectFour/c4imageproc.py
@@ -0,0 +1,26 @@
+'''
+Purpose: to process a board of Connect4
+Authors: Hannah Kolano and Kevin Zhang
+Contact: hannah.kolano@students.olin.edu
+Last edited: 10/7/17
+'''
+
+import rospy
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+#TODO:
+'''get image from Edwin'''
+
+#TODO:
+'''find edges of board, normalize the view'''
+
+#TODO:
+'''Figure out what the current layout is like'''
+
+#TODO:
+'''for debugging, create visual output of board'''
+
+#TODO:
+'''Format to send to the machine learning part'''
