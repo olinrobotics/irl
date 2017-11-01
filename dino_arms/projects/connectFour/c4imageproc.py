c11aa706e6090c2e882340894c9f98ee5e97e105
hkolano <hannah.kolano@students.olin.edu>
2017-11-01 17:56:31 -0400

    some form of contour detection

--- dino_arms/projects/connectFour/c4imageproc.py
+++ dino_arms/projects/connectFour/c4imageproc.py
@@ -2,7 +2,7 @@
 Purpose: to process a board of Connect4
 Authors: Hannah Kolano and Kevin Zhang
 Contact: hannah.kolano@students.olin.edu
-Last edited: 10/7/17
+Last edited: 11/1/17
 '''
 
 import rospy
@@ -10,36 +10,36 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+'''initialize camera'''
 cap = cv2.VideoCapture(0)
+_, frame = cap.read()
+imgray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # grayscale image
 
-while(cap.isOpened()):
-    # Take each frame
-    _, frame = cap.read()
+'''define template'''
+template = cv2.imread('templatepic.png')
+template = cv2.resize(template, (500,500))
+templategray = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY)
 
-    # convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+img = cv2.medianBlur(imgray,5)
+th1 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,20)
+__, contours, __ = cv2.findContours(th1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(frame, contours, -1, (0,255,0), 1)
 
-    # define range of blue color in HSV
-    lower_white = np.array([0, 0, 0])
-    upper_white = np.array([255, 50, 255])
+'''
+cnt = contours[0]
+rect = cv2.minAreaRect(cnt)
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+cv2.drawContours(th1,[box],0,(0,0,255),2)
+'''
 
-    # Threshold the SSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_white, upper_white)
-
-    #Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame, frame, mask=mask)
-
+while(True):
     cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
-    cv2.imshow('res', res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
-        break
-
-cv2.destroyAllWindows()
-
-#TODO:
-'''get image from Edwin'''
+        destroyAllWindows()
+    elif k == ord('s'):
+        cv2.imwrite('tempcontour.png', img)
 
 #TODO:
 '''find edges of board, normalize the view'''
