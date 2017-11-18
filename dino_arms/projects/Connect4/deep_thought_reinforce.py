8bfe8c7d868e9635c07c167355e52b9d128b0191
Kevin Zhang <kzhang8850@gmail.com>
2017-11-18 13:55:59 -0500

    small fix

--- dino_arms/projects/Connect4/deep_thought_reinforce.py
+++ dino_arms/projects/Connect4/deep_thought_reinforce.py
@@ -65,7 +65,7 @@ class Reinforce(object):
 
 
     def run(self):
-        parallel = Parallel(n_jobs=48)
+        parallel = Parallel(n_jobs=-1)
         for episode in range(2000):
             print episode
             game_aftermath = parallel(delayed(play_game)(self.RL1, self.RL2, self.env) for i in range(50))
