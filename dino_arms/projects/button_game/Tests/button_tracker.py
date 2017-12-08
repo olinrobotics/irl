aeb34e6151ca9c822ffbf4a2d70055c4f19769cf
Mark Goldwater <mark.goldwater@students.olin.edu>
2017-12-08 17:06:09 -0500

    returns array of button info now

--- dino_arms/projects/button_game/Tests/button_tracker.py
+++ dino_arms/projects/button_game/Tests/button_tracker.py
@@ -8,132 +8,163 @@ from irl.msg import blue_button, yellow_button, red_button, green_button, all_bu
 import numpy as np
 import cv2
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
+"""
+- Written by: Mark Goldwater
+- What it does: Tracks location and radius of red, green, blue, and yellow buttons
+and publishes them to 'button data' topic
+- Dependencies: You need the custom messages I created (after from irl.msg) in you CMakeLists.txt file
+- How to Run: Run individually using 'python button_tracker.py'
+"""
 
-rospy.init_node('somethingorwhatever')
-buttons = rospy.Publisher('button_data', all_buttons, queue_size=10)
+class buttonTracker(object):
 
-blue_button = blue_button()
-red_button = red_button()
-green_button = green_button()
-yellow_button = yellow_button()
+    """
+    Trackes the buttons and publishes their information to a topic
+    """
 
-all_buttons = all_buttons()
+    def __init__(self):
+        #self.node = rospy.init_node('somethingorwhatever')
+        #Eself.buttonPublisher = rospy.Publisher('button_data', all_buttons, queue_size=10)
 
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+        #self.blue_button = blue_button()
+        #self.red_button = red_button()
+        #self.green_button = green_button()
+        #self.yellow_button = yellow_button()
 
-blueLower = (110, 50, 50)
-blueUpper = (130, 255, 255)
+        #self.all_buttons = all_buttons()
 
-yellowLower = (23, 41, 133)
-yellowUpper = (40, 150, 255)
+        self.greenLower = (29, 86, 6)
+        self.greenUpper = (64, 255, 255)
 
-redLower = (160, 140, 50)
-redUpper = (179, 255, 255)
+        self.blueLower = (110, 50, 50)
+        self.blueUpper = (130, 255, 255)
 
-# grab the reference to the webcam
-camera = cv2.VideoCapture(0)
+        self.yellowLower = (23, 41, 133)
+        self.yellowUpper = (40, 150, 255)
 
-#keep looping
-while True:
-    #grab the current frame
-    (grabbed, frame) = camera.read()
+        self.redLower = (160, 140, 50)
+        self.redUpper = (179, 255, 255)
 
-    # resize the frame, blur it, and return
-    # it in the HSV color space
-    frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        # grab the reference to the webcam
+        self.camera = cv2.VideoCapture(0)
 
-    # create a mask for the color green and then
-    # perform erodions and dialations to make the
-    # tracking more smooth
+    def run(self):
+        #keep looping
 
-    mask_blue = cv2.inRange(hsv, blueLower, blueUpper)
-    mask_green = cv2.inRange(hsv, greenLower, greenUpper)
-    mask_yellow = cv2.inRange(hsv, yellowLower, yellowUpper)
-    mask_red = cv2.inRange(hsv, redLower, redUpper)
+        allButtons = []
 
-    masks = [mask_blue, mask_green, mask_yellow, mask_red]
+        #grab the current frame
+        (grabbed, frame) = self.camera.read()
 
-    # mask_total = mask_blue + mask_green + mask_yellow + mask_red"""
+        # resize the frame, blur it, and return
+        # it in the HSV color space
+        frame = imutils.resize(frame, width=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    for i in range(4):
-        masks[i] = cv2.erode(masks[i], None, iterations=2)
-        masks[i] = cv2.dilate(masks[i], None, iterations=2)
+        # create a mask for the color green and then
+        # perform erodions and dialations to make the
+        # tracking more smooth
 
-        # find contours in the mask and initialize the (x, y) position
-        # of the center of the ball
-        cnts = cv2.findContours(masks[i].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
+        mask_blue = cv2.inRange(hsv, self.blueLower, self.blueUpper)
+        mask_green = cv2.inRange(hsv, self.greenLower, self.greenUpper)
+        mask_yellow = cv2.inRange(hsv, self.yellowLower, self.yellowUpper)
+        mask_red = cv2.inRange(hsv, self.redLower, self.redUpper)
 
-        # only proceed if at least one contour found
-        if len(cnts) > 0:
-            # find largest contour area, compute minimum enclosing circle
-            # and find its center
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        masks = [mask_blue, mask_green, mask_yellow, mask_red]
 
-            # only proceed if radius meets minimum size
-            if radius > 10:
-                if i == 0:
-                    blue_center = center
-                    blue_radius = radius
+            # mask_total = mask_blue + mask_green + mask_yellow + mask_red"""
 
-                    blue_button.center_x = center[0]
-                    blue_button.center_y = center[1]
-                    blue_button.radius = radius
-                elif i == 1:
-                    green_center = center
-                    green_radius = radius
+        for i in range(4):
+            masks[i] = cv2.erode(masks[i], None, iterations=2)
+            masks[i] = cv2.dilate(masks[i], None, iterations=2)
 
-                    green_button.center_x = center[0]
-                    green_button.center_y = center[1]
-                    green_button.radius = radius
-                elif i == 2:
-                    yellow_center = center
-                    yellow_radius = radius
+            # find contours in the mask and initialize the (x, y) position
+            # of the center of the ball
+            cnts = cv2.findContours(masks[i].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+            center = None
 
-                    yellow_button.center_x = center[0]
-                    yellow_button.center_y = center[1]
-                    yellow_button.radius = radius
-                elif i == 3:
-                    red_center = center
-                    red_radius = radius
+            # only proceed if at least one contour found
+            if len(cnts) > 0:
+                # find largest contour area, compute minimum enclosing circle
+                # and find its center
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-                    red_button.center_x = center[0]
-                    red_button.center_y = center[1]
-                    red_button.radius = radius
+                # only proceed if radius meets minimum size
+                if radius > 10:
+                    if i == 0:
+                        blue_center = center
+                        blue_radius = radius
 
+                        allButtons.append(blue_center)
 
-            all_buttons.blue_button = blue_button
-            all_buttons.green_button = green_button
-            all_buttons.yellow_button = yellow_button
-            all_buttons.red_button = red_button
+                        #self.blue_button.center_x = center[0]
+                        #self.blue_button.center_y = center[1]
+                        #self.blue_button.radius = radius
+                    elif i == 1:
+                        green_center = center
+                        green_radius = radius
 
-            buttons.publish(all_buttons)
+                        allButtons.append(green_center)
 
-            # draw circle outline and cente
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                        #self.green_button.center_x = center[0]
+                        #self.green_button.center_y = center[1]
+                        #self.green_button.radius = radius
+                    elif i == 2:
+                        yellow_center = center
+                        yellow_radius = radius
 
+                        allButtons.append(yellow_center)
 
+                        #self.yellow_button.center_x = center[0]
+                        #self.yellow_button.center_y = center[1]
+                        #self.yellow_button.radius = radius
+                    elif i == 3:
+                        red_center = center
+                        red_radius = radius
 
-    # show frame to screen
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+                        allButtons.append(red_button)
+
+                        #self.red_button.center_x = center[0]
+                        #self.red_button.center_y = center[1]
+                        #self.red_button.radius = radius
 
 
+                #self.all_buttons.blue_button = blue_button
+                #self.all_buttons.green_button = green_button
+                #self.all_buttons.yellow_button = yellow_button
+                #self.all_buttons.red_button = red_button
 
-    # q key will stop loop
-    if key == ord("q"):
-        break
+                #self.buttonPublisher.publish(all_buttons)
 
-# turn off camera and close any open windows
-camera.release()
-cv2.destoryAllWindows
+
+                # draw circle outline and cente
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+
+
+        # show frame to screen
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        print(allButtons)
+        return allButtons
+
+
+        # q key will stop loop
+        #if key == ord("q"):
+        #    break
+
+    # turn off camera and close any open windows
+    #camera.release()
+    #cv2.destoryAllWindows
+
+
+
+if __name__=="__main__":
+    Tracker = buttonTracker()
+    Tracker.run()
