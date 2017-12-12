944a54fc4126f5949627ad77a4fc754c35057184
Kevin Zhang <kzhang8850@gmail.com>
2017-12-12 18:25:16 -0500

    idle done, brain started, connect4 game set up

--- dino_arms/projects/Connect4/baseline/connect4.py
+++ dino_arms/projects/Connect4/baseline/connect4.py
@@ -8,9 +8,7 @@ import random
 import time
 from std_msgs.msg import String, Int16
 import cPickle as pickle
-import argparse
 from game_board import C4Board
-from Queue import *
 from minimax import Minimax
 
 """
@@ -20,7 +18,7 @@ by Kevin Zhang and Hannah Kolano
 to run you must connect to Draco, the dragon. see github for how to do that
 
 also requires the following to be run:
-1. roscore
+1. roslaunch ur_modern_driver ur5_bringup.launch robot_ip:=10.42.0.175
 2. rosrun rosserial_python serial_node.py /dev/ttyACM0
 3. rosrun irl ur5_arm_node.py
 
@@ -44,40 +42,117 @@ class Connect4(object):
         self.board = C4Board(render)
         self.max = Minimax()
 
+        rospy.init_node("connect4", anonymous=True)
+        self.ur5_commander = rospy.Publisher("joints_cmd", String, queue_size=10)
+        self.token_grabber = rospy.Publisher("gripper", Int16, queue_size=10)
+        rospy.Subscriber("arm_status", String, self.status_callback)
+        rospy.Subscriber("opponent_move", Int16, self.player_move)
+
+        self.status = True
+        self.turn = "Start"
+        self.player_action = None
+
+
+    def player_move(self, data):
+        """
+        callback from Hannah's CV module, gives back an integer indicating which
+        column the player moved to
+        """
+
+        self.player_action = data.data
+        self.turn = "HUMAN"
+
+
+    def status_callback(self, data):
+        """
+        callback for arm status
+        """
+
+        if data.data == "free":
+            self.status = True
+        else:
+            self.status = False
+
+
+    def check_for_completion(self):
+        """
+        checks to make sure that the arm is done with the previous command
+        before continuing
+        """
+
+        time.sleep(3)
+        while not self.status:
+            pass
+
+
+    def ur5_publish(self, command):
+        """
+        publishes a command, then checks for completion of said command before
+        continuing the script
+        """
+
+        self.ur5_commander.publish(command)
+        self.check_for_completion()
+
+
+    def control_mouth(self):
+        """
+        controls the mouth, opens (0), waits a bit, then closes (1)
+        """
+
+        self.gripper.publish(0)
+        time.sleep(3)
+        self.gripper.publish(1)
+
+
+    def make_move(self, move):
+        """
+        makes a physical move with the physical board, which includes getting a token,
+        then moving to drop it into the chosen column, then returning to home position
+        """
+
+        self.ur5_publish("c4_load_token")
+        self.control_mouth()
+
+        self.ur5_publish("c4_move_"+str(move))
+        self.control_mouth()
+        self.ur5_publish("c4_home")
+
 
     def play_game(self):
         """
         plays a single game of connect 4
         """
 
-        #TODO make the move poses for Draco, whether it be XYZ or joints, there will be 7
-
         self.board.reset()
 
         while True:
-            player_action = None
-            while player_action not in range(1,8):
-                player_action = int(raw_input("YOUR TURN, WHAT'S YOUR MOVE? (1-7) "))
-                print "INVALID MOVE" if player_action not in range(1,8) else ""
 
-            observation_, done = self.board.step(player_action-1, 1)
+            print "WAITING FOR OPPONENT'S MOVE"
+            while self.turn != "HUMAN":
+                pass
 
-            # break if I win
+            observation_, done = self.board.step(self.player_action-1, 1)
+
+            # break if human wins
             if done:
                 print "\n"*2
                 print "HUMAN WINS"
-                break
+                return "HUMAN"
 
+            self.turn = "AI"
             move, value = self.max.bestMove(5, observation_)
-            observation_, done = self.board.step(move, 2)
+            _, done = self.board.step(move, 2)
+
+            print "I MOVED TO COLUMN", str(move+1)
+
+            self.make_move(move+1)
 
             # break if ai wins
             if done:
                 print "\n"*2
                 print "AI WINS"
-                break
-
-        print "GAME OVER"
+                return "AI"
 
 
     def run(self):
@@ -85,15 +160,18 @@ class Connect4(object):
         main interaction sequence
         """
 
-        #TODO need a start game pose
+        self.ur5_publish("c4_start")
+        self.ur5_publish("c4_home")
 
-        #TODO need a rest pose for during the game
+        result = self.play_game()
 
-        self.play_game()
+        if result == "AI":
+            command = "c4_win"
+        else:
+            command = "c4_loss"
 
-        #TODO need a game over for Draco's wins
-        #TODO need a game over for Draco's loss
-        #TODO end game pose
+        self.ur5_publish(command)
+        self.ur5_publish("c4_end")
 
 
 if __name__ == "__main__":
---
+++ dino_arms/projects/brain_F17.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+import numpy as np
+from std_msgs import String, Int16
+
+#TODO tons of imports
+from idle import Idler
+
+
+class The_Brain(object):
+
+    def __init__(self):
+
+
+# TODO lots of stuffs
+
+    def run(self):
+
+
+
+
+
+if __name__=="__main__":
+    brain = The_Brain()
+    brain.run()
