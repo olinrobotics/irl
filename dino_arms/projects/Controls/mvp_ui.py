85da979fe803631bd56623d8c12fba5fd5385d20
Kevin Zhang <kzhang8850@gmail.com>
2018-02-11 11:59:30 -0500

    UR5 set up and ready to go for both arms, dumb brain started

---
+++ dino_arms/projects/Controls/mvp_ui.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+import numpy as np
+import pandas as pd
+import random
+import time
+from std_msgs.msg import String, Int16
+
+
+class UI(object):
+
+
+    def __init__(self):
+
+
+    def setup(self):
+
+
+    def run(self):
---
+++ dino_arms/projects/Planning/dumb_brain.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+import numpy as np
+import pandas as pd
+import random
+import time
+from std_msgs.msg import String, Int16
+
+
+class Dumb_Brain(object):
+
+
+    def __init__(self):
+
+
+    def init_ui(self):
+
+
+    def run(self):
+
+
+
+
+if __name__=="__main__":
