65810fd9f3aa39a412c819f98a7fa9ad86442d53
Kevin Zhang <kzhang8850@gmail.com>
2018-02-25 10:59:26 -0500

    edwin back in for a sec

--- dino_arms/projects/Planning/tf_tutorial4.py
+++ dino_arms/projects/Planning/tf_tutorial4.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
-output = tf.mul(input1, input2)
+output = tf.multiply(input1, input2)
 
 with tf.Session() as sess:
-    sess.run(output)
+    print sess.run(output, feed_dict={input1:[7.], input2:[2.]})
