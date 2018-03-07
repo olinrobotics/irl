c73ce178011b67f8f45d6f05c11b9fac5f6e35c2
Kevin Zhang <kzhang8850@gmail.com>
2018-03-07 18:30:56 -0500

    still chugging on rnn sorting

--- dino_arms/projects/Planning/nn_sort.py
+++ dino_arms/projects/Planning/nn_sort.py
@@ -7,7 +7,7 @@ import numpy as np
 
 learning_rate = .005
 seq_len = 15
-input_size = 1
+input_size = 100
 timesteps = 15
 hidden_size = 128
 class_size = 15
@@ -27,6 +27,12 @@ bias = {
     "out": tf.Variable(tf.random_normal([class_size]))
 }
 
+# def encode(X, seq_len, vocab_size):
+#     x = np.zeros((len(X),seq_len, vocab_size), dtype=np.float32)
+#     for ind,batch in enumerate(X):
+#         for j, elem in enumerate(batch):
+#             x[ind, j, elem] = 1
+#     return x
 
 def batch_gen():
     x = np.zeros((batch_size, seq_len, max_no), dtype=np.float32)
@@ -35,40 +41,65 @@ def batch_gen():
     X = np.random.randint(max_no, size=(batch_size, seq_len))
     Y = np.sort(X, axis=1)
 
+
     for ind, batch in enumerate(X):
         for j, elem in enumerate(batch):
             x[ind, j, elem] = 1
 
-    for ind, batch in enumerate(Y):
-        for j, elem in enumerate(batch):
-            y[ind, j, elem] = 1
+    # for ind, batch in enumerate(Y):
+    #     for j, elem in enumerate(batch):
+    #         y[ind, j, elem] = 1
+    y = Y
+    return x, y
 
-    print x
-    print y
 
+def lstm_cell():
+    return tf.contrib.rnn.BasicLSTMCell(hidden_size, forget_bias=1.0)
 
-def create_model(x, weights, bias):
-
+def RNN(x, weights, bias):
+    # print x.shape
     x = tf.unstack(x, timesteps, 1)
-    lstm_cell = tf.contrib.rnn.BasicLSTMCell(hidden_size, forget_bias=1.0)
-    outputs, states = tf.contrib.rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+    # print "x", x
+    # x = tf.reshape(x, [-1, timesteps])
+    stacked_lstm = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(n_layers)])
 
+    outputs, final_state = tf.contrib.rnn.static_rnn(cell=stacked_lstm, inputs=x, dtype=tf.float32)
+    print "outputs", outputs[-1].shape
     return tf.matmul(outputs[-1], weights["out"]) + bias["out"]
 
-logits = create_model(x_var, weights, bias)
-prediction = tf.nn.softmax(logits)
 
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_var))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-trainer = optimizer.minimize(loss_op)
 
-correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+logits = RNN(x_var, weights, bias)
+# prediction = tf.nn.softmax(logits)
+print "logits", logits.shape
+
+print "shape", y_var.shape
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_var))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+trainer = optimizer.minimize(cost)
+
+# correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_var, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 init = tf.global_variables_initializer()
 
-with tf.Sessions as sess:
-    
+with tf.Session() as sess:
+    sess.run(init)
+
+    for step in range(training_steps):
+        trainx, trainy = batch_gen()
+        sess.run(trainer, feed_dict={x_var:trainx, y_var:trainy})
+
+        if step%100 == 0:
+            loss = sess.run([cost], feed_dict={x_var:trainx, y_var:trainy})
+            print "Step " + str(step) + ", Minibatch Loss= " + "{:.4f}".format(loss[0]) #+ ", Training Accuracy= " + "{:.3f}".format(acc)
+
+
+    print "Training done"
+
+
+
+
 
 
 
---
+++ dino_arms/projects/Planning/tf_tutorials/keras_test.py
@@ -0,0 +1,145 @@
+
+'''
+A sequence to sequence learning implementation for sorting a list of numbers.
+-----------------------------------------------------
+usage: sort_seq.py [-h] [-c] [-v] epochs
+positional arguments:
+  epochs            set number of training epochs
+optional arguments:
+  -h, --help        show this help message and exit
+  -c, --continuing  continue training the saved model
+  -v, --verbose     verbose mode
+  -----------------------------------------------------
+'''
+from __future__ import print_function
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--continuing', action='store_true',
+                    help='continue training the saved model')
+parser.add_argument('-v', '--verbose', action='store_true',
+                    help='verbose mode')
+parser.add_argument('epochs', type=int,
+                    help='set number of training epochs')
+args = parser.parse_args()
+if args.verbose:
+    verbose = True
+else:
+    verbose = False
+epochs = args.epochs
+
+import os
+import numpy as np
+
+# suppress the TensorFlow warning
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+from keras.models import Sequential
+from keras.layers.core import Activation, RepeatVector, Dense, Dropout
+from keras.layers.wrappers import TimeDistributed
+from keras.layers import LSTM
+from keras.models import load_model
+
+
+class colors:
+    ok = '\033[92m'
+    fail = '\033[91m'
+    close = '\033[0m'
+
+
+# encode a given integer sequence into RNN compatible format (one-hot representation)
+def encode(X,seq_len, vocab_size):
+    x = np.zeros((len(X),seq_len, vocab_size), dtype=np.float32)
+    for ind,batch in enumerate(X):
+        for j, elem in enumerate(batch):
+            x[ind, j, elem] = 1
+    return x
+
+
+# generate a stream of inputs for training
+def batch_gen(batch_size=32, seq_len=10, max_no=100):
+    # Randomly generate a batch of integer sequences (X) and its sorted counterpart (Y)
+    x = np.zeros((batch_size, seq_len, max_no), dtype=np.float32)
+    y = np.zeros((batch_size, seq_len, max_no), dtype=np.float32)
+
+    while True:
+	# Generates a batch of input
+        X = np.random.randint(max_no, size=(batch_size, seq_len))
+        Y = np.sort(X, axis=1)
+
+        for ind,batch in enumerate(X):
+            for j, elem in enumerate(batch):
+                x[ind, j, elem] = 1
+
+        for ind,batch in enumerate(Y):
+            for j, elem in enumerate(batch):
+                y[ind, j, elem] = 1
+
+        yield x, y
+        x.fill(0.0)
+        y.fill(0.0)
+
+
+def create_model(seq_len, max_no, n_layers, hidden_size):
+    model = Sequential()
+    # the encoder LSTM
+    model.add(LSTM(hidden_size, input_shape=(seq_len, max_no)))
+    # in next layer, repeat the input seq_len times
+    model.add(RepeatVector(seq_len))
+    # decoder RNN, which will return output sequence
+    for _ in range(n_layers):
+        model.add(LSTM(hidden_size, return_sequences=True))
+    model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(max_no)))
+    model.add(Activation('softmax'))
+    return model
+
+
+if __name__ == "__main__":
+
+    batch_size=32
+    seq_len = 15 # number of elements in sequence to sort
+    max_no = 100 # upper range of the numbers in sequence
+    hidden_size = 128
+    n_layers = 2
+
+    # To continue training from a previous model
+    if args.continuing:
+        print('Continuing training from loaded model...')
+        model = load_model('model.h5')
+    else:
+        model = create_model(seq_len, max_no, n_layers, hidden_size)
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam',
+                      metrics=['accuracy'])
+
+    for ind,(X,Y) in enumerate(batch_gen(batch_size, seq_len, max_no)):
+        print(X.shape)
+        print(Y.shape)
+        loss, acc = model.train_on_batch(X, Y)
+        # check the model performance after each 100th iteration
+        if ind % 100 == 0:
+            testX = np.random.randint(max_no, size=(1, seq_len))
+            test = encode(testX, seq_len, max_no) # one-hot encoded version
+            loss_str = '{:4.3f}'.format(loss)
+            acc_str = '{:4.3f}'.format(acc)
+            # print(f'Epoch: {ind:{8}} of {epochs}  -- loss: {loss_str}, accuracy: {acc_str}')
+
+            if verbose:
+                y = model.predict(test, batch_size=1)
+                np_sorted = np.sort(testX)[0]
+                rnn_sorted = np.argmax(y, axis=2)[0]
+                is_equal = np.array_equal(np_sorted, rnn_sorted)
+                if is_equal:
+                    print(colors.ok+'-----CORRECTLY SORTED-----'+colors.close)
+                else:
+                    print(colors.fail+'-----incorrectly sorted-----'+colors.close)
+
+                print(np_sorted, ': sorted by NumPy algorithm')
+                print(rnn_sorted, ': sorted by trained RNN')
+                print("\n")
+
+        if ind > epochs:
+            # Save trained model
+            model.save('model.h5')
+            break
