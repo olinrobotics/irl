c46283a3e497d9d991c0f4b28f83c6a148d8e3d4
Kevin Zhang <kzhang8850@gmail.com>
2018-03-27 21:56:41 -0400

    starting up RL

---
+++ dino_arms/projects/Planning/RL/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+import rospy
+import numpy as np
+from std_msgs.msg import String, Int16
+import time
+from irl.msg import Cube, Structure
+from cube import Digital_Cube
+import itertools
+
+
+class Main(object):
