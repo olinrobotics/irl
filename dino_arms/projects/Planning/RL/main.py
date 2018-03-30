0fb5738d14bcf940366a4ae526f9ac64b0a6f1df
Kevin Zhang <kzhang8850@gmail.com>
2018-03-30 19:33:51 -0400

    RL up and running, no idea if it's working though

--- dino_arms/projects/Planning/RL/main.py
+++ dino_arms/projects/Planning/RL/main.py
@@ -10,6 +10,7 @@ from cube import Digital_Cube
 from env_RL import RL_environment
 from RL import RL_brain
 import itertools
+import cPickle as pickle
 
 
 class Main(object):
@@ -18,13 +19,13 @@ class Main(object):
         self.env = RL_environment()
         self.RL = RL_brain()
         self.accuracy = 0
-        self.trials = 1000
+        self.trials = 10000000
         self.trial_finished = False
         self.observation = None
         self.action = None
         self.reward = None
-        self.observation_ = None
-        self.test_interval = 100
+        self.observation2 = None
+        self.test_interval = 1000
 
     def run(self):
 
@@ -34,26 +35,32 @@ class Main(object):
             print "EPISODE", i
             while not self.trial_finished:
 
+                # print "IN MAIN OBSERVATION BEFORE CHOOSE", str(self.observation)
                 self.action = self.RL.choose_action(str(self.observation))
 
-                self.observation_, self.reward, self.trial_finished = self.env.step(self.action)
+                self.observation2, self.reward, self.trial_finished = self.env.step(self.action)
 
-                self.RL.learn(str(self.observation, self.action, self.reward, self.observation_))
+                # print "IN MAIN OBSERVATION BEFORE LEARN", str(self.observation)
+                self.RL.learn(str(self.observation), self.reward, str(self.observation2))
+
+            self.RL.update_params()
 
             if i%self.test_interval == 0:
+                accuracy = 0
                 for _ in range(10):
                     self.observation = self.env.reset()
                     self.trial_finished = False
                     while not self.trial_finished:
                         self.action = self.RL.choose_action(str(self.observation))
-                        self.observation_, self.reward, self.trial_finished = self.env.step(self.action)
+                        self.observation, self.reward, self.trial_finished = self.env.step(self.action)
                     self.accuracy += 1 if self.reward == 1 else 0
-                print "TEST ACCRUACY", self.accuracy /= 10
-
-
+                print "TEST ACCRUACY", self.accuracy / 10.0
 
         print "FINISHED TRAINING"
+        with open('/home/rooster/catkin_ws/src/irl/dino_arms/projects/Planning/RL/memory.txt', 'wb') as f:
+            pickle.dump(self.RL.q_table, f)
 
+        print "MEMORY SAVED"
 
 if __name__ == "__main__":
     main = Main()
