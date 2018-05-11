bbe72f10a740fea6d77721a850e92df714a12064
Yichen Jiang <pauljyc0929@gmail.com>
2018-05-11 16:34:37 -0400

    change wait

--- dino_arms/projects/Planning/Coordination/message_sender.py
+++ dino_arms/projects/Planning/Coordination/message_sender.py
@@ -21,14 +21,63 @@ class MessageSender(object):
     """
     def __init__(self):
         rospy.init_node("message_sender")
+        tcp_ip = rospy.get_param("~robot_ip")
+        arm_dict = {'10.42.0.175':'pollux','10.42.0.54':'castor'}
+        self.name = arm_dict[tcp_ip]
 
-        self.cmd_sub = rospy.Subscriber("/build_cmd", Cube_Structures, self.send_msg, queue_size=1)
-        # self.cmd_sub = rospy.Subscriber("/test_input", String, self.send_msg, queue_size=1)
+        self.cmd_sub = rospy.Subscriber("/build_cmd", Cube_Structures, self.send_cube_msg, queue_size=1)
+        if self.name =='castor':
+            self.castor_status_sub = rospy.Subscriber("/coordination_status_castor", String, self.send_castor_status_msg, queue_size=1)
+        elif self.name == 'pollux':
+            self.pollux_status_sub = rospy.Subscriber("/coordination_status_pollux", String, self.send_pollux_status_msg, queue_size=1)
         self.is_sending = False
         self.cube_building = Cube_Structures()
 
-    def send_msg(self, data):
-        # TODO: figure out how to parse data for Cube_Structures type
+    def send_castor_status_msg(self,data):
+
+        smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+        smtp_ssl_port = 465
+        username = 'arms.irl2018@gmail.com'
+        password = 'PolluxCastor2018'
+        sender = 'arms.irl2018@gmail.com'
+        targets = ['arms.irl2018@gmail.com']
+
+        info = '/coordination_status_castor' + ',' + str(data.data)
+
+        msg = MIMEText(info)
+        msg['Subject'] = 'arm_msg ' + time.ctime()
+        msg['From'] = sender
+        msg['To'] = ', '.join(targets)
+
+        server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+        server.login(username, password)
+        server.sendmail(sender, targets, msg.as_string())
+        print ('Castor Status Message Sent Successfully!')
+        server.quit()
+
+    def send_pollux_status_msg(self,data):
+
+        smtp_ssl_host = 'smtp.gmail.com'  # smtp.mail.yahoo.com
+        smtp_ssl_port = 465
+        username = 'arms.irl2018@gmail.com'
+        password = 'PolluxCastor2018'
+        sender = 'arms.irl2018@gmail.com'
+        targets = ['arms.irl2018@gmail.com']
+
+        info = '/coordination_status_pollux' + ',' + str(data.data)
+
+        msg = MIMEText(info)
+        msg['Subject'] = 'arm_msg ' + time.ctime()
+        msg['From'] = sender
+        msg['To'] = ', '.join(targets)
+
+        server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+        server.login(username, password)
+        server.sendmail(sender, targets, msg.as_string())
+        print ('Pollux Status Message Sent Successfully!')
+        server.quit()
+
+    def send_cube_msg(self, data):
         self.cube_building = data
         print("Data: ")
         print(str(self.cube_building))
@@ -42,7 +91,7 @@ class MessageSender(object):
         sender = 'arms.irl2018@gmail.com'
         targets = ['arms.irl2018@gmail.com']
 
-        info = ''
+        info = '/build_cmd,'
         for cube in real_building:
             info = info + str(cube.x) + ','
             info = info + str(cube.y) + ','
@@ -60,7 +109,7 @@ class MessageSender(object):
         server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
         server.login(username, password)
         server.sendmail(sender, targets, msg.as_string())
-        print ('Message Sent Successfully!')
+        print ('Cube Message Sent Successfully!')
         server.quit()
 
 
