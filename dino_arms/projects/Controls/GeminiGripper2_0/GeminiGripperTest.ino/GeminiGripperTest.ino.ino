6c6afbb8ffdd926a9a8dabe512c290406c01d684
Kevin Zhang <kzhang8850@gmail.com>
2018-05-12 15:10:49 -0400

    fixed hand code

--- dino_arms/projects/Controls/GeminiGripper2_0/GeminiGripperTest.ino/GeminiGripperTest.ino.ino
+++ dino_arms/projects/Controls/GeminiGripper2_0/GeminiGripperTest.ino/GeminiGripperTest.ino.ino
@@ -7,69 +7,46 @@
 //servo library
 #include <Servo.h>
 
-//ROS stuff
-#include <ros.h>
-#include <std_msgs/Int32.h>
-
-
 Servo myservo;  //creates servo object "myservo"
 int pos = 90;    // variable to store the servo position
 const int servoPin = 9;
 
-ros::NodeHandle gemini_claw;
-int command = 0; //user input. Later make ROS input.
-
-
-void callback( const std_msgs::Int32& pyMessage) { 
-  command = pyMessage.data;
-  Serial.print(command);
-
-  digitalWrite(LED_BUILTIN, HIGH);
-    
-  change_grip();
-    
-  command = 0;
-    
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(10);
-}
-
-ros::Subscriber<std_msgs::Int32> sub("/grab_cmd", &callback);
 
 void setup() {
   myservo.attach(servoPin);    // attaches the servo on pin 9 to the servo object
   pinMode(LED_BUILTIN, OUTPUT); //blinky builtin light
 
-  gemini_claw.getHardware() -> setBaud(9600);
-  gemini_claw.initNode();
-  gemini_claw.subscribe(sub);
+ 
   delay(500);
   Serial.begin(9600);   //start communication with serial port
+
+   // Toggle between these two blocks of code to custom move the gripper in/out
+
+   // THIS CLOSES IT
+      rest_to_inwards(60);
+      delay(1400);
+      slow_to_rest(60);
+    
+
+  //THIS OPENS IT
+//      rest_to_outwards(120);
+//      delay(1200);
+//      slow_to_rest(120);
+
+
+  
+    delay(300);
 }
 
 void loop() {
-  gemini_claw.spinOnce();
-  delay(1);
+
+ 
 }
 
 /*
   Functions
 */
 
-void change_grip() {
-  if (command == 1) {
-      rest_to_inwards(60);
-      delay(1475);
-      slow_to_rest(60);
-    }
-    if (command == 2) {
-      rest_to_outwards(120);
-      delay(1200);
-      slow_to_rest(120);
-    }
-    //myservo.write(90);
-    delay(300);
-}
 
 //starting at rest, go to out_speed
 void rest_to_outwards(int out_speed) {
